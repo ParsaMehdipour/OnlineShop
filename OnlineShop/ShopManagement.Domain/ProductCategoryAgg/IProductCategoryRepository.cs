75ecd27ef87123d101945c80a73b2f7725c3fedc
Parsa <mehdipourparsa@gmail.com>
2021-05-20 11:52:51 +0430

    feat : add i product category repository interface

---
+++ OnlineShop/ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory productCategory);
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
+    }
+}
