f3729579915a313bc675d4dff25e8375d4138102
Parsa <mehdipourparsa@gmail.com>
2021-05-20 11:53:28 +0430

    feat : add base entity class

---
+++ OnlineShop/0_Framework/Domain/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0_Framework.Domain
+{
+    public class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public BaseEntity()
+        {
+            CreationDate = DateTime.Now;
+        }
+    }
+
+    public class BaseEntity : BaseEntity<long>
+    {
+
+    }
+}
