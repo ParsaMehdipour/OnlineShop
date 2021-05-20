9c884eade61b0d8578d83a25dbd0a8b0771470d1
Parsa <mehdipourparsa@gmail.com>
2021-05-20 11:52:29 +0430

    feat : add productcategory domain

---
+++ OnlineShop/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -0,0 +1,44 @@
+﻿using _0_Framework.Domain;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public class ProductCategory : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Picture { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+        public string KeyWords { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string Slug { get; private set; }
+
+        //Making A New Product Category
+        public ProductCategory(string name, string description, string picture, string pictureAlt,
+            string pictureTitle, string keyWords, string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            KeyWords = keyWords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+
+        //Editing An Existing Product Category
+        public void Edit(string name, string description, string picture, string pictureAlt,
+            string pictureTitle, string keyWords, string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            KeyWords = keyWords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+    }
+}
