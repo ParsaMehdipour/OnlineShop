7cd87c5ec84fc68fa27bb2fe4b7d384ab883102b
Parsa <mehdipourparsa@gmail.com>
2021-05-20 12:19:28 +0430

    feat : add i product category application

---
+++ OnlineShop/ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public interface IProductCategoryApplication
+    {
+        void Create(CreateProductCategory command);
+        void Edit(EditProductCategory command);
+        Domain.ProductCategoryAgg.ProductCategory GetDetails(long id);
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+    }
+}
