3906c50042f90a952922d8531585a0c2f8895031
Parsa <mehdipourparsa@gmail.com>
2021-05-23 01:13:23 +0430

    feat : add operation result class

---
+++ OnlineShop/0_Framework/Application/OperationResult.cs
@@ -0,0 +1,27 @@
+﻿namespace _0_Framework.Application
+{
+    public class OperationResult
+    {
+        public string Message { get; set; }
+        public bool IsSuccedded { get; set; }
+
+        public OperationResult()
+        {
+            IsSuccedded = false;
+        }
+
+        public OperationResult Succedded(string message = "عملیت با موفقیت انجام شد")
+        {
+            IsSuccedded = true;
+            Message = message;
+            return this;
+        }
+
+        public OperationResult Failed(string message = "عملیات با شکست مواجه شد. لطفا مجددا تلاش کنید")
+        {
+            IsSuccedded = false;
+            message = Message;
+            return this;
+        }
+    }
+}
