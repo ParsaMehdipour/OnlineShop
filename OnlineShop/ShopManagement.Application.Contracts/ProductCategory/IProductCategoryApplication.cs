9fa9e17e9ec693f97f1f457c734a8b1cdbb247e0
Parsa <mehdipourparsa@gmail.com>
2021-05-23 01:11:23 +0430

    refactor : changed return types of create , edit , get details method

--- OnlineShop/ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
+++ OnlineShop/ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
+using _0_Framework.Application;
 
 namespace ShopManagement.Application.Contracts.ProductCategory
 {
     public interface IProductCategoryApplication
     {
-        void Create(CreateProductCategory command);
-        void Edit(EditProductCategory command);
-        Domain.ProductCategoryAgg.ProductCategory GetDetails(long id);
+        OperationResult Create(CreateProductCategory command);
+        OperationResult Edit(EditProductCategory command);
+        EditProductCategory GetDetails(long id);
         List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
     }
 }
