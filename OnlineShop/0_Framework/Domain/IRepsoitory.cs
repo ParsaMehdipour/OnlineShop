2357e553dd47f60b47ee982e7c77364c83846f74
Parsa <mehdipourparsa@gmail.com>
2021-05-27 02:22:22 +0430

    feat : add irepository generic interface

---
+++ OnlineShop/0_Framework/Domain/IRepsoitory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace _0_Framework.Domain
+{
+    public interface IRepsoitory<TKey,TEntity> where TEntity : class
+    {
+        TEntity GetById(TKey id);
+        List<TEntity> Get();
+        void Create(TEntity entity);
+        bool Exists(Expression<Func<TEntity, bool>> expression);
+        void SaveChanges();
+    }
+}
