1ad39b7962880110fa94b972e690c8d3592167e7
Parsa <mehdipourparsa@gmail.com>
2021-05-27 11:48:05 +0430

    feat add bootsrapper class

---
+++ OnlineShop/ShopManagement.Configuration/ShopManagementBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _0_Framework.Domain;
+using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Application.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EfCore;
+using ShopManagement.Infrastructure.EfCore.Repositories;
+
+namespace ShopManagement.Configuration
+{
+    public static class ShopManagementBootstrapper
+    {
+        public static void Configure(this IServiceCollection services,string connectionString)
+        {
+            services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+            services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+
+            services.AddDbContext<ShopContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
