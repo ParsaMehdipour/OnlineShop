305e28f41d81cbbfe8dcd2a84e39aa2b75d03743
Parsa <mehdipourparsa@gmail.com>
2021-06-02 19:20:13 +0430

    feat : add index page for product category admin panel

---
+++ OnlineShop/ServiceHost/Areas/AdminPanel/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ServiceHost.Areas.AdminPanel.Pages.Shop.ProductCategories
+{
+    public class IndexModel : PageModel
+    {
+        public ProductCategorySearchModel SearchModel;
+        private readonly IProductCategoryApplication _application;
+        public List<ProductCategoryViewModel> ProductCategories;
+
+        public IndexModel(IProductCategoryApplication application)
+        {
+            _application = application;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            ProductCategories = _application.Search(searchModel);
+        }
+    }
+}
