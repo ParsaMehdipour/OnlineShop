c29fa9ea24894b4b8a5d20345ce78905299ffbfd
Parsa <mehdipourparsa@gmail.com>
2021-06-09 02:04:15 +0430

    feat : add product domain

---
+++ OnlineShop/ShopManagement.Domain/ProductAgg/Product.cs
@@ -0,0 +1,73 @@
+﻿using _0_Framework.Domain;
+using _0_Framework.Infrastructure;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Domain.ProductAgg
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+        public double UnitPrice { get; private set; }
+        public bool IsInStock { get; private set; }
+        public string ShortDescription { get; private set; }
+        public string Description { get; private set; }
+        public string Picture { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+        public string Slug { get; private set; }
+        public string Keywords { get; private set; }
+        public string MetaDescription { get; private set; }
+        public long CategoryId { get; private set; }
+        public ProductCategory ProductCategory { get; private set; }
+
+        public Product(string name, string code, double unitPrice,
+            string shortDescription, string description, string picture,
+            string pictureAlt, string pictureTitle, string slug,
+            string keywords, string metaDescription, long categoryId)
+        {
+            Name = name;
+            Code = code;
+            UnitPrice = unitPrice;
+            ShortDescription = shortDescription;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Slug = slug;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            CategoryId = categoryId;
+            IsInStock = true;
+        }
+
+        public void Edit(string name, string code, double unitPrice,
+            string shortDescription, string description, string picture,
+            string pictureAlt, string pictureTitle, string slug,
+            string keywords, string metaDescription, long categoryId)
+        {
+            Name = name;
+            Code = code;
+            UnitPrice = unitPrice;
+            ShortDescription = shortDescription;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Slug = slug;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            CategoryId = categoryId;
+        }
+
+        public void InStock()
+        {
+            IsInStock = true;
+        }
+
+        public void NotInStock()
+        {
+            IsInStock = false;  
+        }
+    }
+}
