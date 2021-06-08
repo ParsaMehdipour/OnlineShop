b0b32985777bafaf657995b5cea58e44c461eed3
Parsa <mehdipourparsa@gmail.com>
2021-06-09 02:05:37 +0430

    feat : add product view model class

---
+++ OnlineShop/ShopManagement.Application.Contracts/Product/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopManagement.Application.Contracts.Product
+{
+    public class ProductViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public double UnitPrice { get; set; }
+        public string Category { get; set; }
+    }
+}
