59baa980582bf95273c4d1915d293fbefef955dc
Parsa <mehdipourparsa@gmail.com>
2021-06-09 13:38:21 +0430

    feat : add application messages

---
+++ OnlineShop/0_Framework/Application/ApplicationMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0_Framework.Application
+{
+    public class ApplicationMessages
+    {
+        public const string DuplicatedRecord = "امکان ایجاد رکورد تکراری وجود ندارد. لظفا دوباره تلاش کنید";
+        public const string RecordNotFound = "رکورد با اطلاعات درخواست داده شده وجود ندارد. لطفا دوباره تلاش کنید";
+    }
+}
