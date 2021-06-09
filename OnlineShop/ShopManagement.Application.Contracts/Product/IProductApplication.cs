ae04541c9ef5056060cc4055e5adb0c09a4f8de0
Parsa <mehdipourparsa@gmail.com>
2021-06-09 13:39:23 +0430

    feat : add i product application interface

---
+++ OnlineShop/ShopManagement.Application.Contracts/Product/IProductApplication.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.Product
+{
+    public interface IProductApplication
+    {
+        OperationResult Create(CreateProduct command);
+        OperationResult Edit(EditProduct command);
+        List<ProductViewModel> Search(ProductSearchModel searchModel);
+        EditProduct GetDetails(long id);
+        OperationResult InStock(long id);
+        OperationResult OutOfStock(long id);
+    }
+}
