527b97e5758c255dbdeaa4e3e145f1f5a5d9061c
Parsa <mehdipourparsa@gmail.com>
2021-06-09 13:39:42 +0430

    refactor : add needed methods

--- OnlineShop/ShopManagement.Domain/ProductAgg/IProductRepository.cs
+++ OnlineShop/ShopManagement.Domain/ProductAgg/IProductRepository.cs
@@ -1,8 +1,12 @@
-﻿using _0_Framework.Domain;
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Product;
 
 namespace ShopManagement.Domain.ProductAgg
 {
     public interface IProductRepository : IRepsoitory<long , Product>
     {
+        EditProduct GetDetails(long id);
+        List<ProductViewModel> Search(ProductSearchModel searchModel);
     }
 }
