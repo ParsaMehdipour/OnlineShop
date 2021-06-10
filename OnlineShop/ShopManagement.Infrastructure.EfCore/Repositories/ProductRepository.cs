e9e4e9a0dd89d3c3e49021fda4048744084b320f
Parsa <mehdipourparsa@gmail.com>
2021-06-10 16:43:26 +0430

    feat : add product repository

---
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/Repositories/ProductRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Domain.ProductAgg;
+
+namespace ShopManagement.Infrastructure.EfCore.Repositories
+{
+    public class ProductRepository : BaseRepsitory<long, Product>, IProductRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditProduct GetDetails(long id)
+        {
+            return _context.Products.Select(x => new EditProduct()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Code = x.Code,
+                UnitPrice = x.UnitPrice,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                CategoryId = x.CategoryId,
+                Slug = x.Slug,
+                ShortDescription = x.ShortDescription,
+                Description = x.Description,
+                MetaDescription = x.MetaDescription,
+                Keywords = x.Keywords
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ProductViewModel> Search(ProductSearchModel searchModel)
+        {
+            var query = _context.Products
+                .Include(x => x.ProductCategory)
+                .Select(x => new ProductViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Category = x.ProductCategory.Name,
+                    Code = x.Code
+                });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Code))
+                query = query.Where(x => x.Code.Contains(searchModel.Code));
+
+            if (searchModel.CategoryId != 0)
+                query = query.Where(x => x.Id == searchModel.CategoryId);
+
+            return query.OrderByDescending(x => x.Id).ToList();
+        }
+    }
+}
