8ba451a24966ee41f1b2e7d298813e302b42e129
Parsa <mehdipourparsa@gmail.com>
2021-06-10 16:42:59 +0430

    feat : add product mapping

---
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/Mapping/ProductMapping.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopManagement.Domain.ProductAgg;
+
+namespace ShopManagement.Infrastructure.EfCore.Mapping
+{
+    public class ProductMapping : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(x => x.Code).HasMaxLength(15)
+                .IsRequired();
+
+            builder.Property(x => x.ShortDescription).HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(x => x.Picture).HasMaxLength(1000);
+
+            builder.Property(x => x.PictureAlt).HasMaxLength(255);
+
+            builder.Property(x => x.PictureTitle).HasMaxLength(500);
+
+            builder.Property(x => x.Keywords).HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.MetaDescription).HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(x => x.Slug).HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(x => x.UnitPrice);
+
+            builder.Property(x => x.InStock);
+
+            builder.Property(x => x.Description);
+
+            builder.HasOne(x => x.ProductCategory)
+                .WithMany(x => x.Products)
+                .HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
