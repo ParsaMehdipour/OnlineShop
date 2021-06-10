3ab87341f3be184a0faa9e1a945945ae61e9f0b8
Parsa <mehdipourparsa@gmail.com>
2021-06-10 16:43:43 +0430

    refactor : add products dbset

--- OnlineShop/ShopManagement.Infrastructure.EfCore/ShopContext.cs
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/ShopContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Domain.ProductCategoryAgg;
 using ShopManagement.Infrastructure.EfCore.Mapping;
 
@@ -7,6 +8,7 @@ namespace ShopManagement.Infrastructure.EfCore
     public class ShopContext : DbContext
     {
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         public ShopContext(DbContextOptions<ShopContext> options) : base(options)
         {
