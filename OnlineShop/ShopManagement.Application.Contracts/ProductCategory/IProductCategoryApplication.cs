cbb020cc08d0952d6533a8cfb979711a602d32dc
Parsa <mehdipourparsa@gmail.com>
2021-06-12 12:45:36 +0430

    refactor : add get product categories signature to i product applicationinterface

--- OnlineShop/ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
+++ OnlineShop/ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -9,5 +9,6 @@ namespace ShopManagement.Application.Contracts.ProductCategory
         OperationResult Edit(EditProductCategory command);
         EditProductCategory GetDetails(long id);
         List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+        List<ProductCategoryViewModel> GetProductCategories();
     }
 }
