ca0084dba4eb5d07f2fef1755cf22dbea5db854c
Parsa <mehdipourparsa@gmail.com>
2021-06-12 12:44:44 +0430

    refactor : add missing properties to product view model

--- OnlineShop/ShopManagement.Application.Contracts/Product/ProductViewModel.cs
+++ OnlineShop/ShopManagement.Application.Contracts/Product/ProductViewModel.cs
@@ -7,5 +7,7 @@
         public string Code { get; set; }
         public double UnitPrice { get; set; }
         public string Category { get; set; }
+        public string CreationDate { get; set; }
+        public string Picture { get; set; }
     }
 }
