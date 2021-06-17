0fe3097e8f9ab305250e466f3be6461aa2d63ac6
Parsa <mehdipourparsa@gmail.com>
2021-06-17 14:58:06 +0430

    refactor : add data anotations for create product

--- OnlineShop/ShopManagement.Application.Contracts/Product/CreateProduct.cs
+++ OnlineShop/ShopManagement.Application.Contracts/Product/CreateProduct.cs
@@ -1,22 +1,41 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.ProductCategory;
 
 namespace ShopManagement.Application.Contracts.Product
 {
     public class CreateProduct
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Code { get; set; }
+
         public double UnitPrice { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string ShortDescription { get; set; }
+
         public string Description { get; set; }
+
         public string Picture { get; set; }
+
         public string PictureAlt { get; set; }
+
         public string PictureTitle { get; set; }
+
         public string Slug { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Keywords { get; set; }
+
         public string MetaDescription { get; set; }
+
         public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public List<ProductCategoryViewModel> Categories { get; set; }
     }
 }
