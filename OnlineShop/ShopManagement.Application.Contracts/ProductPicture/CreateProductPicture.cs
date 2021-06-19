f062d764ae1caf8c116bb61b336c34d57461eb33
Parsa <mehdipourparsa@gmail.com>
2021-06-19 13:37:43 +0430

    feat : add create product picture class

---
+++ OnlineShop/ShopManagement.Application.Contracts/ProductPicture/CreateProductPicture.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.ProductPicture
+{
+    public class CreateProductPicture
+    {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public long ProductId { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string Picture { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string PictureTitle { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string PictureAlt { get; set; }
+    }
+}
