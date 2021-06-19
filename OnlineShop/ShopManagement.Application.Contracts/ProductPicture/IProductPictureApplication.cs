818118729696cf71c33a79fd913842fd22844296
Parsa <mehdipourparsa@gmail.com>
2021-06-19 13:38:41 +0430

    feat : add i product picture application interface

---
+++ OnlineShop/ShopManagement.Application.Contracts/ProductPicture/IProductPictureApplication.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.ProductPicture
+{
+    public interface IProductPictureApplication
+    {
+        List<ProductPictureViewModel> Search(ProductPictureSearchModel pictureSearchModel);
+        OperationResult Restore(long id);
+        OperationResult Remove(long id);
+        OperationResult Create(CreateProductPicture command);
+        OperationResult EditProductPicture(EditProductPicture command);
+        EditProductPicture GetDetails(long id);
+
+    }
+}
