0fde7135d348b065fea12dfddfc771d517dff171
Parsa <mehdipourparsa@gmail.com>
2021-06-20 11:54:20 +0430

    refactor : add product pictures dbset

--- OnlineShop/ShopManagement.Infrastructure.EfCore/ShopContext.cs
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/ShopContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Domain.ProductPictureAgg;
 using ShopManagement.Infrastructure.EfCore.Mapping;
 
 namespace ShopManagement.Infrastructure.EfCore
@@ -9,6 +10,7 @@ namespace ShopManagement.Infrastructure.EfCore
     {
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductPicture> ProductPictures { get; set; }
 
         public ShopContext(DbContextOptions<ShopContext> options) : base(options)
         {
