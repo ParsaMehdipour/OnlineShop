fc9d15f09e61a500439ebed3e0e1ca9d21d76141
Parsa <mehdipourparsa@gmail.com>
2021-06-21 13:29:24 +0430

    refactor : add get products method to i product repository interface

--- OnlineShop/ShopManagement.Domain/ProductAgg/IProductRepository.cs
+++ OnlineShop/ShopManagement.Domain/ProductAgg/IProductRepository.cs
@@ -8,5 +8,6 @@ namespace ShopManagement.Domain.ProductAgg
     {
         EditProduct GetDetails(long id);
         List<ProductViewModel> Search(ProductSearchModel searchModel);
+        List<ProductViewModel> GetProducts();
     }
 }
