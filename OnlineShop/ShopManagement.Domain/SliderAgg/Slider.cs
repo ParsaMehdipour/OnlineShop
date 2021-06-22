b99d551537830a0285258adfde142ec97f1e372a
Parsa <mehdipourparsa@gmail.com>
2021-06-22 14:18:05 +0430

    feat : add slider domain class

---
+++ OnlineShop/ShopManagement.Domain/SliderAgg/Slider.cs
@@ -0,0 +1,49 @@
+﻿using _0_Framework.Domain;
+
+namespace ShopManagement.Domain.SliderAgg
+{
+    public class Slider : BaseEntity
+    {
+        public string Picture { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+        public string Heading { get; private set; }
+        public string Title { get; private set; }
+        public string Text { get; private set; }
+        public string BtnText { get; private set; }
+        public bool IsRemoved { get; private set; }
+
+        public Slider(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText)
+        {
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Heading = heading;
+            Title = title;
+            Text = text;
+            BtnText = btnText;
+            IsRemoved = false;
+        }
+
+        public void Edit(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText)
+        {
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Heading = heading;
+            Title = title;
+            Text = text;
+            BtnText = btnText;
+        }
+
+        public void Remove()
+        {
+            IsRemoved = true;
+        }
+
+        public void Restore()
+        {
+            IsRemoved = false;
+        }
+    }
+}
