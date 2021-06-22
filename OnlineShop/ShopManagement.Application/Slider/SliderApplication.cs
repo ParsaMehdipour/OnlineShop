2cca30abd3c83087fad0d50d6009ab27515d5890
Parsa <mehdipourparsa@gmail.com>
2021-06-22 14:16:35 +0430

    feat : add slider application class

---
+++ OnlineShop/ShopManagement.Application/Slider/SliderApplication.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+using ShopManagement.Application.Contracts.Slider;
+using ShopManagement.Domain.SliderAgg;
+
+namespace ShopManagement.Application.Slider
+{
+    public class SliderApplication : ISliderApplication
+    {
+        private readonly ISliderRepository _repository;
+
+        public SliderApplication(ISliderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public OperationResult Create(CreateSlider command)
+        {
+            var result = new OperationResult();
+
+            var slider = new Domain.SliderAgg.Slider(command.Picture, command.PictureAlt, command.PictureTitle
+                , command.Heading, command.Title, command.Text
+                , command.BtnText);
+
+            _repository.Create(slider);
+
+            _repository.SaveChanges();
+
+            return result.Succedded();
+        }
+
+        public OperationResult Edit(EditSlider command)
+        {
+            var result = new OperationResult();
+
+            var slider = _repository.GetById(command.Id);
+
+            if (slider == null)
+                return result.Failed(ApplicationMessages.RecordNotFound);
+
+            slider.Edit(command.Picture,command.PictureAlt,command.PictureTitle
+            ,command.Heading,command.Title,command.Text
+            ,command.BtnText);
+
+            _repository.SaveChanges();
+
+            return result.Succedded();
+        }
+
+        public OperationResult Remove(long id)
+        {
+            var result = new OperationResult();
+
+            var slider = _repository.GetById(id);
+
+            if (slider == null)
+                return result.Failed(ApplicationMessages.RecordNotFound);
+
+            slider.Remove();
+
+            return result.Succedded();
+        }
+
+        public OperationResult Restore(long id)
+        {
+            var result = new OperationResult();
+
+            var slider = _repository.GetById(id);
+
+            if (slider == null)
+                return result.Failed(ApplicationMessages.RecordNotFound);
+
+            slider.Restore();
+
+            return result.Succedded();
+        }
+
+        public EditSlider GetDetails(long id)
+        {
+            return _repository.GetDetails(id);
+        }
+
+        public List<SliderViewModel> GetList()
+        {
+            return _repository.GetList();
+        }
+    }
+}
