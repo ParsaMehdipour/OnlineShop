d7e6b84fe8b3d5a00dc458f5a5ab788ae9b22c53
Parsa <mehdipourparsa@gmail.com>
2021-06-22 14:17:53 +0430

    feat : add i slider repository interface

---
+++ OnlineShop/ShopManagement.Domain/SliderAgg/ISliderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Slider;
+
+namespace ShopManagement.Domain.SliderAgg
+{
+    public interface ISliderRepository : IRepsoitory<long,Slider>
+    {
+        EditSlider GetDetails(long id);
+        List<SliderViewModel> GetList();
+    }
+}
