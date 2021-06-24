86a532a7e113958550538f4437955df0f81f6eca
Parsa <mehdipourparsa@gmail.com>
2021-06-24 14:04:54 +0430

    refactor : configure slider services

--- OnlineShop/ShopManagement.Configuration/ShopManagementBootstrapper.cs
+++ OnlineShop/ShopManagement.Configuration/ShopManagementBootstrapper.cs
@@ -6,12 +6,15 @@ using Microsoft.Extensions.DependencyInjection;
 using ShopManagement.Application.Contracts.Product;
 using ShopManagement.Application.Contracts.ProductCategory;
 using ShopManagement.Application.Contracts.ProductPicture;
+using ShopManagement.Application.Contracts.Slider;
 using ShopManagement.Application.Product;
 using ShopManagement.Application.ProductCategory;
 using ShopManagement.Application.ProductPicture;
+using ShopManagement.Application.Slider;
 using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Domain.ProductCategoryAgg;
 using ShopManagement.Domain.ProductPictureAgg;
+using ShopManagement.Domain.SliderAgg;
 using ShopManagement.Infrastructure.EfCore;
 using ShopManagement.Infrastructure.EfCore.Repositories;
 
@@ -24,10 +27,12 @@ namespace ShopManagement.Configuration
             services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
             services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
             services.AddTransient<IProductPictureRepository, ProductPictureRepository>();
+            services.AddTransient<ISliderRepository, SliderRepository>();
 
             services.AddTransient<IProductApplication, ProductApplication>();
             services.AddTransient<IProductRepository, ProductRepository>();
             services.AddTransient<IProductPictureApplication, ProductPictureApplication>();
+            services.AddTransient<ISliderApplication, SliderApplication>();
 
             services.AddDbContext<ShopContext>(options =>
             {
