a923283956ab49182c20ed0c5d5ef49099dbdaff
Parsa <mehdipourparsa@gmail.com>
2021-06-24 14:04:07 +0430

    feat : impelement i slider repository

---
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/Repositories/SliderRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Application.Contracts.Slider;
+using ShopManagement.Domain.SliderAgg;
+
+namespace ShopManagement.Infrastructure.EfCore.Repositories
+{
+    public class SliderRepository : BaseRepsitory<long,Slider> , ISliderRepository
+    {
+        private readonly ShopContext _context;
+
+        public SliderRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditSlider GetDetails(long id)
+        {
+            return _context.Sliders.Select(s => new EditSlider
+            {
+                Id = s.Id,
+                Picture = s.Picture,
+                PictureAlt = s.PictureAlt,
+                PictureTitle = s.PictureTitle,
+                Title = s.Title,
+                Text = s.Text,
+                BtnText = s.BtnText,
+                Heading = s.Heading
+
+            }).FirstOrDefault(s => s.Id == id);
+        }
+
+        public List<SliderViewModel> GetList()
+        {
+            return _context.Sliders.Select(s => new SliderViewModel
+            {
+                Title = s.Title,
+                Picture = s.Picture,
+                PictureAlt = s.PictureAlt,
+                PictureTitle = s.PictureTitle,
+                BtnText = s.BtnText,
+                Text = s.Text,
+                Heading = s.Heading
+            }).OrderByDescending(s=>s.Id).ToList();
+        }
+    }
+}
