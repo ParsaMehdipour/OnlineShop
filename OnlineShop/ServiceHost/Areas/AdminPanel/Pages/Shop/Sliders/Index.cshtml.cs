12c744901239fbb76c6cbecbba92ba1123ebbc20
Parsa <mehdipourparsa@gmail.com>
2021-06-26 14:07:56 +0430

    feat : add slide index page

---
+++ OnlineShop/ServiceHost/Areas/AdminPanel/Pages/Shop/Sliders/Index.cshtml.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Application.Contracts.ProductPicture;
+using ShopManagement.Application.Contracts.Slider;
+
+namespace ServiceHost.Areas.AdminPanel.Pages.Shop.Sliders
+{
+    public class IndexModel : PageModel
+    {
+
+        [TempData]
+        public string Message { get; set; }
+        public List<SliderViewModel> Sliders;
+
+        private readonly ISliderApplication _application;
+
+        public IndexModel(ISliderApplication application)
+        {
+            _application = application;
+        }
+
+        public void OnGet()
+        {
+            Sliders = _application.GetList();
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            var command = new CreateSlider();
+
+            return Partial("./Create", command);
+        }
+
+        public JsonResult OnPostCreate(CreateSlider command)
+        {
+            var result = _application.Create(command);
+
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(long id)
+        {
+            var slider = _application.GetDetails(id);
+
+            return Partial("./Edit", slider);
+        }
+
+        public JsonResult OnPostEdit(EditSlider command)
+        {
+            var result = _application.Edit(command);
+
+            return new JsonResult(result);
+        }
+
+
+        public IActionResult OnGetRemove(long id)
+        {
+            var result = _application.Remove(id);
+            if (result.IsSuccedded)
+                return RedirectToPage("./Index");
+
+            Message = result.Message;
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnGetRestore(long id)
+        {
+            var result = _application.Restore(id);
+            if (result.IsSuccedded)
+                return RedirectToPage("./Index");
+
+            Message = result.Message;
+            return RedirectToPage("./Index");
+        }
+    }
+}
