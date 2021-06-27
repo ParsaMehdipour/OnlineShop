e791649fc814d6654c41802340aefce67804edab
Parsa <mehdipourparsa@gmail.com>
2021-06-28 02:03:01 +0430

    feat : add slide query

---
+++ OnlineShop/01_OnlineShopQuery/Contracts/Slide/SlideQuery.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShopManagement.Infrastructure.EfCore;
+
+namespace _01_OnlineShopQuery.Contracts.Slide
+{
+    public class SlideQuery : ISlideQuery
+    {
+        private readonly ShopContext _context;
+
+        public SlideQuery(ShopContext context)
+        {
+            _context = context;
+        }
+        public List<SlideQueryModel> GetSlides()
+        {
+            return _context.Sliders.Where(s => s.IsRemoved == false)
+                .Select(s => new SlideQueryModel
+                {
+                    Title = s.Title,
+                    Picture = s.Picture,
+                    PictureAlt = s.PictureAlt,
+                    PictureTitle = s.PictureTitle,
+                    BtnText = s.BtnText,
+                    Text = s.Text,
+                    Heading = s.Heading,
+                    Link = s.Link
+                }).ToList();
+        }
+    }
+}
