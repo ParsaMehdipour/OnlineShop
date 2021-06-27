4f999a901d08170917279616eb60f700d0c2dda5
Parsa <mehdipourparsa@gmail.com>
2021-06-28 02:04:43 +0430

    feat : add slide view component class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/SlideViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using _01_OnlineShopQuery.Contracts.Slide;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using ShopManagement.Application.Contracts.Slider;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class SlideViewComponent : ViewComponent
+    {
+        private readonly ISlideQuery _query;
+
+        public SlideViewComponent(ISlideQuery query)
+        {
+            _query = query;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var slides = _query.GetSlides();
+
+            return View(slides);
+        }
+    }
+}
