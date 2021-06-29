9e98500af6271b35dd3deb692ba2daffd95c6698
Parsa <mehdipourparsa@gmail.com>
2021-06-29 13:49:26 +0430

    refactor : invoke product category component

--- OnlineShop/ServiceHost/Pages/Index.cshtml.cs
+++ OnlineShop/ServiceHost/Pages/Index.cshtml.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ServiceHost.Pages
 {
@@ -19,7 +14,6 @@ namespace ServiceHost.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
