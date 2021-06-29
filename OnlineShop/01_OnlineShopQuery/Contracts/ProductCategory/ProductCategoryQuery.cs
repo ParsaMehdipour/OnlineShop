b7ce1381a452f4dba94e1a1645587dbac96252cb
Parsa <mehdipourparsa@gmail.com>
2021-06-29 13:48:22 +0430

    feat : add product category query class

---
+++ OnlineShop/01_OnlineShopQuery/Contracts/ProductCategory/ProductCategoryQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShopManagement.Infrastructure.EfCore;
+
+namespace _01_OnlineShopQuery.Contracts.ProductCategory
+{
+    public class ProductCategoryQuery : IProductCategoryQuery
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryQuery(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public List<ProductCategoryQueryModel> GetProductCategoryQueryModels()
+        {
+            return _context.ProductCategories.Select(p => new ProductCategoryQueryModel
+            {
+                Name = p.Name,
+                Picture = p.Picture,
+                PictureAlt = p.PictureAlt,
+                PictureTitle = p.PictureTitle,
+                Slug = p.Slug
+            }).ToList();
+        }
+    }
+}
