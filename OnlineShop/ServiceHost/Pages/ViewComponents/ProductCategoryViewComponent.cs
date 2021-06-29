4b156395f386fefee60557888917a8fd30f9b15e
Parsa <mehdipourparsa@gmail.com>
2021-06-29 13:48:56 +0430

    feat : add product categoru view component class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/ProductCategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using _01_OnlineShopQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class ProductCategoryViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _query;
+
+        public ProductCategoryViewComponent(IProductCategoryQuery query)
+        {
+            _query = query;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var productCategories = _query.GetProductCategoryQueryModels();
+
+            return View(productCategories);
+        }
+    }
+}
