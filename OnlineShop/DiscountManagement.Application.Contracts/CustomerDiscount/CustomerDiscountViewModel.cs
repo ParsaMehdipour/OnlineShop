0fab49913f56ba8cb3853d48e95b89f1b7d317b6
Parsa <mehdipourparsa@gmail.com>
2021-07-01 00:46:05 +0430

    feat : add customer discount view model

---
+++ OnlineShop/DiscountManagement.Application.Contracts/CustomerDiscount/CustomerDiscountViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscountManagement.Application.Contracts.CustomerDiscount
+{
+    public class CustomerDiscountViewModel
+    {
+        public long Id { get; set; }
+        public string Product { get; set; }
+        public int DiscountRate { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Reason { get; set; }
+    }
+}
