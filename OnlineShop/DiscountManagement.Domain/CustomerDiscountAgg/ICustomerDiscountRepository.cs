a546ab5530f66d5e5446b93cdd29050c800fa600
Parsa <mehdipourparsa@gmail.com>
2021-07-01 00:45:15 +0430

    feat : add i customer discount repository interface

---
+++ OnlineShop/DiscountManagement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
+
+namespace DiscountManagement.Domain.CustomerDiscountAgg
+{
+    public interface ICustomerDiscountRepository : IRepsoitory<long , CustomerDiscount>
+    {
+        List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel);
+        EditCustomerDiscount GetDetails(long id);
+    }
+}
