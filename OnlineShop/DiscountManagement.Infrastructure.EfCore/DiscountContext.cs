4aca94a2819605d70592394be8bf96d3c4df15ad
Parsa <mehdipourparsa@gmail.com>
2021-07-02 01:03:29 +0430

    feat : add discount context

---
+++ OnlineShop/DiscountManagement.Infrastructure.EfCore/DiscountContext.cs
@@ -0,0 +1,26 @@
+﻿using DiscountManagement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastructure.EfCore.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountManagement.Infrastructure.EfCore
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<CustomerDiscount> CustomerDiscounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(CustomerDiscountMapping).Assembly;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
