1a14e2a90b42d35dd2f935d02f52291b03acbfc7
Parsa <mehdipourparsa@gmail.com>
2021-07-02 01:03:44 +0430

    feat : add customer discount repository

---
+++ OnlineShop/DiscountManagement.Infrastructure.EfCore/Repository/CustomerDiscountRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EfCore;
+
+namespace DiscountManagement.Infrastructure.EfCore.Repository
+{
+    public class CustomerDiscountRepository :BaseRepsitory<long , CustomerDiscount> , ICustomerDiscountRepository
+    {
+        private readonly DiscountContext _context;
+        private readonly ShopContext _shopContext;
+
+        public CustomerDiscountRepository(DiscountContext context , ShopContext shopContext) : base(context)
+        {
+            _context = context;
+            _shopContext = shopContext;
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+            var products = _shopContext.Products.Select(p => new {p.Id, p.Name}).ToList();
+
+            var query = _context.CustomerDiscounts.Select(c => new CustomerDiscountViewModel
+            {
+                Id = c.Id,
+                DiscountRate = c.DiscountRate,
+                ProductId = c.ProductId,
+                StartDate = c.StartDate.ToString(CultureInfo.InvariantCulture),
+                StartDateGr = c.StartDate,
+                EndDate = c.EndDate.ToString(CultureInfo.InvariantCulture),
+                EndDateGr = c.EndDate,
+                Reason = c.Reason
+            });
+
+            if (searchModel.ProductId > 0)
+                query = query.Where(c => c.ProductId == searchModel.ProductId);
+
+            if (!string.IsNullOrWhiteSpace(searchModel.StartDate))
+            {
+                var startDate = DateTime.Now;
+
+                query = query.Where(c => c.StartDateGr < startDate);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchModel.EndDate))
+            {
+                var endDate = DateTime.Now;
+
+                query = query.Where(c => c.EndDateGr > endDate);
+            }
+
+            var discounts = query.OrderByDescending(c => c.Id).ToList();
+
+            discounts.ForEach(discount => 
+                discount.Product = products.FirstOrDefault(c=>c.Id == discount.ProductId)?.Name);
+        }
+
+        public EditCustomerDiscount GetDetails(long id)
+        {
+            return _context.CustomerDiscounts.Select(c => new EditCustomerDiscount
+            {
+                Id = c.Id,
+                ProductId = c.ProductId,
+                StartDate = c.StartDate.ToString(CultureInfo.InvariantCulture),
+                EndDate = c.EndDate.ToString(CultureInfo.InvariantCulture),
+                Reason = c.Reason,
+                DiscountRate = c.DiscountRate
+            }).FirstOrDefault(c=>c.Id == id);
+        }
+    }
+}
