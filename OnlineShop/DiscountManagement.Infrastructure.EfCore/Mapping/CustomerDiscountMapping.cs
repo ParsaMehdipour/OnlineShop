ebbfa643016c1649909b4020e2b20b97d689d9bc
Parsa <mehdipourparsa@gmail.com>
2021-07-02 01:03:15 +0430

    feat : add customer discount mapping class

---
+++ OnlineShop/DiscountManagement.Infrastructure.EfCore/Mapping/CustomerDiscountMapping.cs
@@ -0,0 +1,25 @@
+﻿using DiscountManagement.Domain.CustomerDiscountAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiscountManagement.Infrastructure.EfCore.Mapping
+{
+    public class CustomerDiscountMapping : IEntityTypeConfiguration<CustomerDiscount>
+    {
+        public void Configure(EntityTypeBuilder<CustomerDiscount> builder)
+        {
+            builder.ToTable("CustomerDiscounts");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.DiscountRate);
+
+            builder.Property(x => x.Reason);
+
+            builder.Property(x => x.StartDate);
+
+            builder.Property(x => x.EndDate);
+
+        }
+    }
+}
