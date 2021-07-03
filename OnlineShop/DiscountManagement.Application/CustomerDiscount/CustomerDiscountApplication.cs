50f44daa8f1424adac956c6e84caa57749cbb3dc
Parsa <mehdipourparsa@gmail.com>
2021-07-03 17:13:35 +0430

    feat : add customer discount application class

---
+++ OnlineShop/DiscountManagement.Application/CustomerDiscount/CustomerDiscountApplication.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+
+namespace DiscountManagement.Application.CustomerDiscount
+{
+    public class CustomerDiscountApplication : ICustomerDiscountApplication
+    {
+        private readonly ICustomerDiscountRepository _repository;
+
+        public CustomerDiscountApplication(ICustomerDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public OperationResult Define(DefineCustomerDiscount command)
+        {
+            var operationResult = new OperationResult();
+
+            if (_repository.Exists(x => x.ProductId == command.ProductId && x.DiscountRate == command.DiscountRate))
+                return operationResult.Failed(ApplicationMessages.DuplicatedRecord);
+
+            var startDate = command.StartDate.ToGeorgianDateTime();
+            var endDate = command.StartDate.ToGeorgianDateTime();
+
+            var customerDiscount = new Domain.CustomerDiscountAgg.CustomerDiscount(
+                command.ProductId, command.DiscountRate, startDate, endDate
+                , command.Reason);
+
+            _repository.Create(customerDiscount);
+
+            _repository.SaveChanges();
+
+            return operationResult.Succedded();
+
+        }
+
+        public OperationResult Edit(EditCustomerDiscount command)
+        {
+            var operationResult = new OperationResult();
+
+            var customerDiscount = _repository.GetById(command.Id);
+
+            if (customerDiscount == null)
+                return operationResult.Failed(ApplicationMessages.RecordNotFound);
+
+            var startDate = command.StartDate.ToGeorgianDateTime();
+            var endDate = command.StartDate.ToGeorgianDateTime();
+
+            if (_repository.Exists(x =>
+                x.ProductId == command.ProductId && x.DiscountRate == command.DiscountRate && x.Id != command.Id)) 
+                return operationResult.Failed(ApplicationMessages.DuplicatedRecord);
+            customerDiscount.Edit(command.Id,command.DiscountRate,startDate,endDate,command.Reason);
+
+            _repository.SaveChanges();
+
+            return operationResult.Succedded();
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+            return _repository.Search(searchModel);
+        }
+
+        public EditCustomerDiscount GetDetails(long id)
+        {
+            return _repository.GetDetails(id);
+        }
+    }
+}
