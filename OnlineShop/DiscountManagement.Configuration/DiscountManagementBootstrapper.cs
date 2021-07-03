f93a9b1f9fc7fd356417c16ea6720349722a6da1
Parsa <mehdipourparsa@gmail.com>
2021-07-03 17:14:30 +0430

    feat : add discount management bootstrapper class

---
+++ OnlineShop/DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using DiscountManagement.Application.Contracts.CustomerDiscount;
+using DiscountManagement.Application.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastructure.EfCore;
+using DiscountManagement.Infrastructure.EfCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscountManagement.Configuration
+{
+    public class DiscountManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services , string connectionString)
+        {
+            services.AddTransient<ICustomerDiscountRepository, CustomerDiscountRepository>();
+
+            services.AddTransient<ICustomerDiscountApplication, CustomerDiscountApplication>();
+
+            services.AddDbContext<DiscountContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
