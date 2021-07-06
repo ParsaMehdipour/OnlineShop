3f582da9b865a15c32cc5ff090cc20d6557cebc7
Parsa <mehdipourparsa@gmail.com>
2021-07-06 20:57:11 +0430

    feat : add colleague discount domain

---
+++ OnlineShop/DiscountManagement.Domain/ColleagueDiscountAgg/ColleagueDiscount.cs
@@ -0,0 +1,35 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountManagement.Domain.ColleagueDiscountAgg
+{
+    public class ColleagueDiscount : BaseEntity
+    {
+        public long ProductId { get; private set; }
+        public long DiscountRate { get; private set; }
+        public bool IsRemoved { get; private set; }
+        //public string Reason { get; private set; }
+
+        public ColleagueDiscount(long productId, long discountRate)
+        {
+            ProductId = productId;
+            DiscountRate = discountRate;
+            IsRemoved = false;
+        }
+
+        public void Edit(long productId, long discountRate)
+        {
+            ProductId = productId;
+            DiscountRate = discountRate;
+        }
+
+        public void Remove()
+        {
+            IsRemoved = true;
+        }
+
+        public void Restore()
+        {
+            IsRemoved = false;
+        }
+    }
+}
