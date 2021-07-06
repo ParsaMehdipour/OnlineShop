c425943b141ee3daae5e847f92b96f39194990b4
Parsa <mehdipourparsa@gmail.com>
2021-07-06 20:59:42 +0430

    feat : add colleague discount mapping class

---
+++ OnlineShop/DiscountManagement.Infrastructure.EfCore/Mapping/ColleagueDiscountMapping.cs
@@ -0,0 +1,26 @@
+﻿using DiscountManagement.Domain.ColleagueDiscountAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiscountManagement.Infrastructure.EfCore.Mapping
+{
+    public class ColleagueDiscountMapping : IEntityTypeConfiguration<ColleagueDiscount>
+    {
+        public void Configure(EntityTypeBuilder<ColleagueDiscount> builder)
+        {
+            builder.ToTable("ColleagueDiscounts");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.ProductId)
+                .IsRequired();
+
+            builder.Property(x => x.IsRemoved);
+
+            builder.Property(x => x.DiscountRate)
+                .IsRequired();
+
+            //builder.Property(x => x.Reason);
+        }
+    }
+}
