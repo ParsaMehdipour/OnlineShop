44d013ed98788359ad8ac0633d30e2d9be7e6d80
Parsa <mehdipourparsa@gmail.com>
2021-07-06 20:58:48 +0430

    feat : add define colleague discount class

---
+++ OnlineShop/DiscountManagement.Application.Contracts/ColleagueDiscount/DefineColleagueDiscount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ShopManagement.Application.Contracts.Product;
+
+namespace DiscountManagement.Application.Contracts.ColleagueDiscount
+{
+    public class DefineColleagueDiscount
+    {
+        public string Product { get; set; }
+        public long ProductId { get; set; }
+        public long DiscountRate { get; set; }
+        //public string Reason { get; set; }
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
