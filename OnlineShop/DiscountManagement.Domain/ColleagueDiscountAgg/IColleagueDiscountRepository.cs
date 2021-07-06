35f84306a85caf95fa6ea07c812b7e10827a672f
Parsa <mehdipourparsa@gmail.com>
2021-07-06 20:57:40 +0430

    feat : add icolleague discount repository interface

---
+++ OnlineShop/DiscountManagement.Domain/ColleagueDiscountAgg/IColleagueDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
+
+namespace DiscountManagement.Domain.ColleagueDiscountAgg
+{
+    public interface IColleagueDiscountRepository : IRepsoitory<long , ColleagueDiscount>
+    {
+        List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel);
+        EditColleagueDiscount GetDetails(long id);
+    }
+}
