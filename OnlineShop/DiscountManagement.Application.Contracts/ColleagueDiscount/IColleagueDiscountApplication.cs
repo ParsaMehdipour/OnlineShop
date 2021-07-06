054e45b28d47120496918c8da9891b3c1394cb45
Parsa <mehdipourparsa@gmail.com>
2021-07-06 20:59:21 +0430

    feat : add i colleague discount application interface

---
+++ OnlineShop/DiscountManagement.Application.Contracts/ColleagueDiscount/IColleagueDiscountApplication.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace DiscountManagement.Application.Contracts.ColleagueDiscount
+{
+    public interface IColleagueDiscountApplication
+    {
+        List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel);
+        EditColleagueDiscount GetDetails(long id);
+        OperationResult Define(DefineColleagueDiscount command);
+        OperationResult Edit(EditColleagueDiscount command);
+        OperationResult Remove(long id);
+        OperationResult Restore(long id);
+    }
+}
