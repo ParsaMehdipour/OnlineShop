c277a86abf8817eb840a5cf7d574088b2a724401
Parsa <mehdipourparsa@gmail.com>
2021-07-06 20:58:24 +0430

    feat : add colleague discount view model class

---
+++ OnlineShop/DiscountManagement.Application.Contracts/ColleagueDiscount/ColleagueDiscountViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DiscountManagement.Application.Contracts.ColleagueDiscount
+{
+    public class ColleagueDiscountViewModel
+    {
+        public long Id { get; set; }
+        public long ProductId { get; set; }
+        public string Product { get; set; }
+        public long DiscountRate { get; set; }
+        //public string Reason { get; set; }
+        public bool IsRemoved { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
