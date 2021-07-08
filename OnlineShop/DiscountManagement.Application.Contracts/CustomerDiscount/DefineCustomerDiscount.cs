a725bfa784f6023d19ccfc3ffae4056781497bc2
Parsa <mehdipourparsa@gmail.com>
2021-07-08 17:57:46 +0430

    refactor : add data anotations

--- OnlineShop/DiscountManagement.Application.Contracts/CustomerDiscount/DefineCustomerDiscount.cs
+++ OnlineShop/DiscountManagement.Application.Contracts/CustomerDiscount/DefineCustomerDiscount.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.Product;
 
 namespace DiscountManagement.Application.Contracts.CustomerDiscount
 {
     public class DefineCustomerDiscount
     {
+        [Range(1, 1000, ErrorMessage = ValidationMessages.IsRequired)]
         public long ProductId { get; set; }
+
+        [Range(1, 99, ErrorMessage = ValidationMessages.IsRequired)]
         public int DiscountRate { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string StartDate { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string EndDate { get; set; }
+
         public string Reason { get; set; }
+
         public List<ProductViewModel> Products { get; set; }
     }
 }
