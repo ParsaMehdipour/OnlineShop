a9567abd8f95586ba33347fbe3be3531f66eddde
Parsa <mehdipourparsa@gmail.com>
2021-07-08 17:58:32 +0430

    feat : add colleague discount index page

---
+++ OnlineShop/ServiceHost/Areas/AdminPanel/Pages/Discounts/ColleagueDiscounts/Index.cshtml.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ServiceHost.Areas.AdminPanel.Pages.Discounts.ColleagueDiscounts
+{
+    public class IndexModel : PageModel
+    {
+        [TempData]
+        public string Message { get; set; }
+        public ColleagueDiscountSearchModel SearchModel;
+        private readonly IColleagueDiscountApplication _application;
+        private readonly IProductApplication _productApplication;
+        public List<ColleagueDiscountViewModel> ColleagueDiscounts;
+        public SelectList Products;
+
+        public IndexModel(IProductApplication productApplication,IColleagueDiscountApplication application)
+        {
+            _application = application;
+            _productApplication = productApplication;
+        }
+
+        public void OnGet(ColleagueDiscountSearchModel searchModel)
+        {
+            Products = new SelectList(_productApplication.GetProducts(), "Id", "Name");
+            ColleagueDiscounts = _application.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            var command = new DefineColleagueDiscount(){Products = _productApplication.GetProducts()};
+
+            return Partial("./Create", command);
+        }
+
+        public JsonResult OnPostCreate(DefineColleagueDiscount command)
+        {
+            var result = _application.Define(command);
+
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(long id)
+        {
+            var colleagueDiscount = _application.GetDetails(id);
+            colleagueDiscount.Products = _productApplication.GetProducts();
+
+            return Partial("./Edit", colleagueDiscount);
+        }
+
+        public JsonResult OnPostEdit(EditColleagueDiscount command)
+        {
+            var result = _application.Edit(command);
+
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetRemove(long id)
+        {
+            var result = _application.Remove(id);
+            if (result.IsSuccedded)
+                return RedirectToPage("./Index");
+
+            Message = result.Message;
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnGetRestore(long id)
+        {
+            var result = _application.Restore(id);
+            if (result.IsSuccedded)
+                return RedirectToPage("./Index");
+
+            Message = result.Message;
+            return RedirectToPage("./Index");
+        }
+    }
+}
