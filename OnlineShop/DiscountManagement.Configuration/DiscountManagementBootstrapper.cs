03b13186182058f916e6d9316865cb6bbf9aac38
Parsa <mehdipourparsa@gmail.com>
2021-07-08 17:58:08 +0430

    refactor : configure colleague discount services

--- OnlineShop/DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
+++ OnlineShop/DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
@@ -1,5 +1,8 @@
-﻿using DiscountManagement.Application.Contracts.CustomerDiscount;
+﻿using DiscountManagement.Application.ColleagueDiscount;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
 using DiscountManagement.Application.CustomerDiscount;
+using DiscountManagement.Domain.ColleagueDiscountAgg;
 using DiscountManagement.Domain.CustomerDiscountAgg;
 using DiscountManagement.Infrastructure.EfCore;
 using DiscountManagement.Infrastructure.EfCore.Repository;
@@ -13,8 +16,10 @@ namespace DiscountManagement.Configuration
         public static void Configure(IServiceCollection services , string connectionString)
         {
             services.AddTransient<ICustomerDiscountRepository, CustomerDiscountRepository>();
+            services.AddTransient<IColleagueDiscountRepository, ColleagueDiscountRepository>();
 
             services.AddTransient<ICustomerDiscountApplication, CustomerDiscountApplication>();
+            services.AddTransient<IColleagueDiscountApplication, ColleagueDiscountApplication>();
 
             services.AddDbContext<DiscountContext>(options =>
             {
