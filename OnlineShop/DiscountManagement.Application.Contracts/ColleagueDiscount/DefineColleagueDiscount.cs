7e2135d649147c9782ac2f309fe8d8c1cc1d74be
Parsa <mehdipourparsa@gmail.com>
2021-07-08 17:57:15 +0430

    refactor : remove unused property

--- OnlineShop/DiscountManagement.Application.Contracts/ColleagueDiscount/DefineColleagueDiscount.cs
+++ OnlineShop/DiscountManagement.Application.Contracts/ColleagueDiscount/DefineColleagueDiscount.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.Product;
 
 namespace DiscountManagement.Application.Contracts.ColleagueDiscount
 {
     public class DefineColleagueDiscount
     {
-        public string Product { get; set; }
+        [Range(1 , 1000 , ErrorMessage = ValidationMessages.IsRequired)]
         public long ProductId { get; set; }
+
+        [Range(1, 99, ErrorMessage = ValidationMessages.IsRequired)]
         public long DiscountRate { get; set; }
+
         //public string Reason { get; set; }
         public List<ProductViewModel> Products { get; set; }
     }
