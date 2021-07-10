6f3c848763570a79b07f51a5345a374b101afdb9
Parsa <mehdipourparsa@gmail.com>
2021-07-10 23:04:59 +0430

    feat  : add inventory class

---
+++ OnlineShop/InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Domain;
+using InventoryManagement.Domain.Operation;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public class Inventory : BaseEntity
+    {
+        public long ProductId { get; private set; } //Which Product ?
+        public double UnitPrice { get; private set; }
+        public bool InStock { get; private set; }
+        public List<InventoryOperation> InventoryOperations { get; private set; }
+
+        public Inventory(long productId, double unitPrice)
+        {
+            ProductId = productId;
+            UnitPrice = unitPrice;
+            InStock = false;
+            InventoryOperations = new List<InventoryOperation>();
+        }
+
+        private long CalculateCurrentCount()
+        {
+            var plus = InventoryOperations.Where(x => x.Operation is true).Sum(x => x.Count);
+            var minus = InventoryOperations.Where(x => x.Operation is false).Sum(x => x.Count);
+
+            return (plus - minus);
+        }
+
+        public void Increase(long count, long operatorId, string description)
+        {
+            var currentCount = CalculateCurrentCount() + count;
+
+            var inventoryOperation = new InventoryOperation(true, count, operatorId, currentCount, description, 0, Id);
+
+            InventoryOperations.Add(inventoryOperation);
+
+            InStock = currentCount > 0;
+        }
+
+        public void Reduce(long count, long operatorId, long orderId, string description)
+        {
+            var currentCount = CalculateCurrentCount() - count;
+
+            var inventoryOperation = new InventoryOperation(true, count, operatorId, currentCount, description, orderId, Id);
+
+            InventoryOperations.Add(inventoryOperation);
+
+            InStock = currentCount > 0;
+        }
+    }
+}
