716df1bd0a69e527da25742a41e21b2d40cc12e9
Parsa <mehdipourparsa@gmail.com>
2021-07-12 01:24:48 +0430

    feat : add reduce inventory class

---
+++ OnlineShop/InventoryManagement.Application.Contracts/Inventory/ReduceInventory.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManagement.Application.Contracts.Inventory
+{
+    public class ReduceInventory
+    {
+        public long Count { get; set; }
+        public long ProductId { get; set; }
+        public long OperatorId { get; set; }
+        public long OrderId { get; set; }
+        public string Description { get; set; }
+    }
+}
