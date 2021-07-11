41e622417c5e8320c4ce18399aed6009549bd4ee
Parsa <mehdipourparsa@gmail.com>
2021-07-12 01:23:51 +0430

    feat : add i inventory application interface

---
+++ OnlineShop/InventoryManagement.Application.Contracts/Inventory/IInventoryApplication.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace InventoryManagement.Application.Contracts.Inventory
+{
+    public interface IInventoryApplication
+    {
+        OperationResult Create(CreateInventory command);
+        OperationResult Edit(EditInventory command);
+        OperationResult Increase(IncreaseInventory command);
+        OperationResult Reduce(List<ReduceInventory> command);
+        OperationResult Reduce(ReduceInventory command);
+        List<InventoryViewModel> Search(InventorySearchModel searchModel);
+        EditInventory GetDetails(long id);
+
+    }
+}
