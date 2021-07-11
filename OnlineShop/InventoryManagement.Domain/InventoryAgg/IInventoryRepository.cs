b37bdd96bb385b1b9e0ae315e983a038e48321c5
Parsa <mehdipourparsa@gmail.com>
2021-07-12 01:21:17 +0430

    feat : add i inventory repository interface

---
+++ OnlineShop/InventoryManagement.Domain/InventoryAgg/IInventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using InventoryManagement.Application.Contracts.Inventory;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public interface IInventoryRepository : IRepsoitory<long, Inventory>
+    {
+        List<InventoryViewModel> Search(InventorySearchModel searchModel);
+        Inventory GetByProductId(long productId);
+        EditInventory GetDetails(long id);
+    }
+}
