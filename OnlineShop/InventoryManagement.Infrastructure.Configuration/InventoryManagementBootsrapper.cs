ad2d4f08724005f51b99935534cbac1572fb5db7
Parsa <mehdipourparsa@gmail.com>
2021-07-13 02:13:20 +0430

    feat : add inventory management bootstrapper class

---
+++ OnlineShop/InventoryManagement.Infrastructure.Configuration/InventoryManagementBootsrapper.cs
@@ -0,0 +1,21 @@
+﻿using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EfCore;
+using InventoryManagement.Infrastructure.EfCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InventoryManagement.Infrastructure.Configuration
+{
+    public class InventoryManagementBootsrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IInventoryRepository, InventoryRepository>();
+
+            services.AddDbContext<InventoryContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
