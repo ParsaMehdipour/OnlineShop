494e75d182bc70a63b7d1e659f39220a1b583aa4
Parsa <mehdipourparsa@gmail.com>
2021-07-13 02:13:54 +0430

    feat : add inventory context

---
+++ OnlineShop/InventoryManagement.Infrastructure.EfCore/InventoryContext.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EfCore.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.EfCore
+{
+    public class InventoryContext : DbContext
+    {
+
+        public DbSet<Inventory> Inventory { get; set; }
+
+        public InventoryContext(DbContextOptions options) 
+            :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(InventoryMapping).Assembly;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
