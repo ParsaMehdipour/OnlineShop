3d1c8ed8e6a53d1375f8860a268830fe741a1e75
Parsa <mehdipourparsa@gmail.com>
2021-07-16 02:13:24 +0430

    feat : add inventory index page

---
+++ OnlineShop/ServiceHost/Areas/AdminPanel/Pages/Inventory/Index.cshtml.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using InventoryManagement.Application.Contracts.Inventory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ServiceHost.Areas.AdminPanel.Pages.Inventory
+{
+    public class IndexModel : PageModel
+    {
+        [TempData]
+        public string Message { get; set; }
+        public InventorySearchModel SearchModel;
+        private readonly IInventoryApplication _application;
+        private readonly IProductApplication _productApplication;
+        public List<InventoryViewModel> InventoryViewModels;
+        public SelectList Products;
+
+        public IndexModel(IProductApplication productApplication,IInventoryApplication application)
+        {
+            _application = application;
+            _productApplication = productApplication;
+        }
+
+        public void OnGet(InventorySearchModel searchModel)
+        {
+            Products = new SelectList(_productApplication.GetProducts(), "Id", "Name");
+            InventoryViewModels = _application.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            var command = new CreateInventory{Products = _productApplication.GetProducts()};
+
+            return Partial("./Create", command);
+        }
+
+        public JsonResult OnPostCreate(CreateInventory command)
+        {
+            var result = _application.Create(command);
+
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(long id)
+        {
+            var inventory = _application.GetDetails(id);
+            inventory.Products = _productApplication.GetProducts();
+
+            return Partial("./Edit", inventory);
+        }
+
+        public JsonResult OnPostEdit(EditInventory command)
+        {
+            var result = _application.Edit(command);
+
+            return new JsonResult(result);
+        }
+    }
+}
