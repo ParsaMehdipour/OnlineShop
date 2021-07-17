ff4cc7cf1b48e133328c25c18d9506e3282aaae6
Parsa <mehdipourparsa@gmail.com>
2021-07-18 01:41:11 +0430

    refactor : add get log method  to i inventory repository interface

--- OnlineShop/InventoryManagement.Domain/InventoryAgg/IInventoryRepository.cs
+++ OnlineShop/InventoryManagement.Domain/InventoryAgg/IInventoryRepository.cs
@@ -9,5 +9,6 @@ namespace InventoryManagement.Domain.InventoryAgg
         List<InventoryViewModel> Search(InventorySearchModel searchModel);
         Inventory GetByProductId(long productId);
         EditInventory GetDetails(long id);
+        List<InventoryOperationViewModel> GetLog(long id);
     }
 }
