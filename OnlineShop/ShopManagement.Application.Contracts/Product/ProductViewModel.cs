691d6a2d68b824df41cbfa541bbd57c56cc3df9f
Parsa <mehdipourparsa@gmail.com>
2021-07-19 00:22:31 +0430

    refactor : remove unit price and in stock field from product view model class

--- OnlineShop/ShopManagement.Application.Contracts/Product/ProductViewModel.cs
+++ OnlineShop/ShopManagement.Application.Contracts/Product/ProductViewModel.cs
@@ -4,11 +4,9 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public string Code { get; set; }
-        public double UnitPrice { get; set; }
         public string Category { get; set; }
         public string CreationDate { get; set; }
+        public string Code { get; set; }
         public string Picture { get; set; }
-        public bool IsInStock { get; set; }
     }
 }
