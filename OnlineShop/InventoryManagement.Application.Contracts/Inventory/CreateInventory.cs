d0cce38261138297c1f38a5e59f183e6bbf45793
Parsa <mehdipourparsa@gmail.com>
2021-07-19 00:18:33 +0430

    refactor : add validation to create inventory

--- OnlineShop/InventoryManagement.Application.Contracts/Inventory/CreateInventory.cs
+++ OnlineShop/InventoryManagement.Application.Contracts/Inventory/CreateInventory.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.Product;
 
 namespace InventoryManagement.Application.Contracts.Inventory
 {
     public class CreateInventory
     {
+        [Range(1,10000,ErrorMessage = ValidationMessages.IsRequired)]
         public long ProductId { get; set; }
+
+        [Range(1, double.MaxValue, ErrorMessage = ValidationMessages.IsRequired)]
         public double UnitPrice { get; set; }
+
         public List<ProductViewModel> Products { get; set; }
     }
 }
