affda777abc2760881a3247f1d3d9525411225b7
Parsa <mehdipourparsa@gmail.com>
2021-07-21 23:32:59 +0430

    refactor : add relation to product category query model class

--- OnlineShop/01_OnlineShopQuery/Contracts/ProductCategory/ProductCategoryQueryModel.cs
+++ OnlineShop/01_OnlineShopQuery/Contracts/ProductCategory/ProductCategoryQueryModel.cs
@@ -1,11 +1,16 @@
-﻿namespace _01_OnlineShopQuery.Contracts.ProductCategory
+﻿using System.Collections.Generic;
+using _01_OnlineShopQuery.Contracts.Product;
+
+namespace _01_OnlineShopQuery.Contracts.ProductCategory
 {
     public class ProductCategoryQueryModel
     {
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Picture { get; set; }
         public string PictureAlt { get; set; }
         public string PictureTitle { get; set; }
         public string Slug { get; set; }
+        public List<ProductQueryModel> Products { get; set; }
     }
 }
