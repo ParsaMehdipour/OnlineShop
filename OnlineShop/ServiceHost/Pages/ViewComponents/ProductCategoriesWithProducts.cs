ec5bcfad74306a271be4d9dff32321f3eb7a010f
Parsa <mehdipourparsa@gmail.com>
2021-07-21 23:34:00 +0430

    feat : add product categories with products class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/ProductCategoriesWithProducts.cs
@@ -0,0 +1,22 @@
+﻿using _01_OnlineShopQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class ProductCategoriesWithProducts : ViewComponent
+    {
+        private readonly IProductCategoryQuery _query;
+
+        public ProductCategoriesWithProducts(IProductCategoryQuery query)
+        {
+            _query = query;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var query = _query.GetProductCategoryWithProducts();
+
+            return View(query);
+        }
+    }
+}
