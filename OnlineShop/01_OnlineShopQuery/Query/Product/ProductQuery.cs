7665a56b748d37649b7d18aaa85645a4581745f0
Parsa <mehdipourparsa@gmail.com>
2021-07-24 23:28:02 +0430

    feat : add product query class

---
+++ OnlineShop/01_OnlineShopQuery/Query/Product/ProductQuery.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Application;
+using _01_OnlineShopQuery.Contracts.Product;
+using DiscountManagement.Infrastructure.EfCore;
+using InventoryManagement.Infrastructure.EfCore;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EfCore;
+
+namespace _01_OnlineShopQuery.Query.Product
+{
+    public class ProductQuery : IProductQuery
+    {
+        private readonly ShopContext _shopContext;
+        private readonly DiscountContext _discountContext;
+        private readonly InventoryContext _inventoryContext;
+
+        public ProductQuery(ShopContext shopContext,DiscountContext discountContext,InventoryContext inventoryContext)
+        {
+            _shopContext = shopContext;
+            _discountContext = discountContext;
+            _inventoryContext = inventoryContext;
+        }
+
+        public List<ProductQueryModel> GetLatestProducts()
+        {
+            var inventories = _inventoryContext.Inventory.Select(x => new {x.UnitPrice, x.ProductId}).ToList();
+            var discounts = _discountContext.CustomerDiscounts.Select(x => new {x.DiscountRate, x.ProductId}).ToList();
+            var products = _shopContext.Products
+                .Include(x => x.ProductCategory)
+                .Select(x => new ProductQueryModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Category = x.ProductCategory.Name,
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    Slug = x.Slug
+                }).OrderByDescending(x => x.Id).Take(6).ToList();
+
+            foreach (var product in products)
+            {
+                var productInventory = inventories.FirstOrDefault(x => x.ProductId == product.Id);
+
+                if (productInventory != null)
+                {
+                    var price = productInventory.UnitPrice;
+
+                    product.Price = price.ToMoney();
+
+                    var productDiscount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
+
+                    if (productDiscount != null)
+                    {
+                        var discountRate = productDiscount.DiscountRate;
+
+                        product.DiscountRate = discountRate;
+                        product.HasDiscount = discountRate > 0;
+
+                        var discountAmount = Math.Round((price * discountRate) / 100);
+
+                        product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+}
