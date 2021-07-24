d3d025b2c4b3cce19b5994251dede13088dece02
Parsa <mehdipourparsa@gmail.com>
2021-07-24 23:27:45 +0430

    feat : add i product query interface

---
+++ OnlineShop/01_OnlineShopQuery/Contracts/Product/IProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace _01_OnlineShopQuery.Contracts.Product
+{
+    public interface IProductQuery
+    {
+        List<ProductQueryModel> GetLatestProducts();
+    }
+}
