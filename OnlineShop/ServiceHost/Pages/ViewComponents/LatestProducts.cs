224b96e5915c127229f7d141fe1656712c6002e2
Parsa <mehdipourparsa@gmail.com>
2021-07-24 23:29:15 +0430

    feat : add latest products class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/LatestProducts.cs
@@ -0,0 +1,23 @@
+﻿using _01_OnlineShopQuery.Contracts.Product;
+using _01_OnlineShopQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class LatestProducts : ViewComponent
+    {
+        private readonly IProductQuery _query;
+
+        public LatestProducts(IProductQuery query)
+        {
+            _query = query;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var query = _query.GetLatestProducts();
+
+            return View(query);
+        }
+    }
+}
