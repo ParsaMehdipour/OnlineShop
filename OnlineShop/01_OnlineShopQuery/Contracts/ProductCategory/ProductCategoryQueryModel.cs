27addd8e988757ce891612e6f12d3af0b5aaf55c
Parsa <mehdipourparsa@gmail.com>
2021-07-28 23:04:36 +0430

    feat : add get produtc with products method to product category query class

--- OnlineShop/01_OnlineShopQuery/Contracts/ProductCategory/ProductCategoryQueryModel.cs
+++ OnlineShop/01_OnlineShopQuery/Contracts/ProductCategory/ProductCategoryQueryModel.cs
@@ -11,6 +11,9 @@ namespace _01_OnlineShopQuery.Contracts.ProductCategory
         public string PictureAlt { get; set; }
         public string PictureTitle { get; set; }
         public string Slug { get; set; }
+        public string Description { get; set; }
+        public string MetaDescription { get; set; }
+        public string Keywords { get; set; }
         public List<ProductQueryModel> Products { get; set; }
     }
 }
