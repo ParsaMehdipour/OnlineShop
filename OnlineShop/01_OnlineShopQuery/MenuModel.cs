4cdbad6ce92eb72df7073ab48cf7e224a9b94b9c
Parsa <mehdipourparsa@gmail.com>
2021-07-29 23:50:42 +0430

    feat : add menu model class

---
+++ OnlineShop/01_OnlineShopQuery/MenuModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using _01_OnlineShopQuery.Contracts.ProductCategory;
+
+namespace _01_LampshadeQuery
+{
+    public class MenuModel
+    {
+        public List<ProductCategoryQueryModel> ProductCategories { get; set; }
+    }
+}
