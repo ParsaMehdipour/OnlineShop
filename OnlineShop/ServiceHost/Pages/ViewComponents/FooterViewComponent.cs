e169777d03f96047f55917135e1d0fc13142dc8c
Parsa <mehdipourparsa@gmail.com>
2021-07-29 23:51:49 +0430

    feat : add footer view component class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
