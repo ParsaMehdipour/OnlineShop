e91d5d4c60fe63345b33030578641a68f7fa7f3c
Parsa <mehdipourparsa@gmail.com>
2021-07-29 23:51:57 +0430

    feat : add menu view component class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using _01_LampshadeQuery;
+using _01_OnlineShopQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _query;
+
+        public MenuViewComponent(IProductCategoryQuery query)
+        {
+            _query = query;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = new MenuModel()
+            {
+                ProductCategories = _query.GetProductCategoryQueryModels()
+            };
+
+            return View(result);
+        }
+    }
+}
