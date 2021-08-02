073fc6b78be539ec9a4872ada334bba1b55a1cbe
Parsa <mehdipourparsa@gmail.com>
2021-08-02 21:52:29 +0430

    refactor : add max file size validation message

--- OnlineShop/0_Framework/Application/ValidationMessages.cs
+++ OnlineShop/0_Framework/Application/ValidationMessages.cs
@@ -3,5 +3,6 @@
     public class ValidationMessages
     {
         public const string IsRequired = "این فیلد نمی تواند خالی باشد";
+        public const string MaxFileSize = "فایل حجیم تر از حد مجاز است";
     }
 }
