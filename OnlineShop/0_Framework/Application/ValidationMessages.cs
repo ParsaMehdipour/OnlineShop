5eb2b49cc5c691bffb60efc4db868d9527481370
Parsa <mehdipourparsa@gmail.com>
2021-08-03 18:15:08 +0430

    refactor : add invalid  file format validation message

--- OnlineShop/0_Framework/Application/ValidationMessages.cs
+++ OnlineShop/0_Framework/Application/ValidationMessages.cs
@@ -1,8 +1,9 @@
 ﻿namespace _0_Framework.Application
 {
-    public class ValidationMessages
+    public static class ValidationMessages
     {
         public const string IsRequired = "این فیلد نمی تواند خالی باشد";
         public const string MaxFileSize = "فایل حجیم تر از حد مجاز است";
+        public const string InvalidFileFormat = "فرمت فایل مجاز نمی باشد ";
     }
 }
