33c1336f69fede9205f05d09c22331131f4d1416
Parsa <mehdipourparsa@gmail.com>
2021-08-08 23:41:19 +0430

    refactor : add get product with category to i product repository interface

--- OnlineShop/ShopManagement.Domain/ProductAgg/IProductRepository.cs
+++ OnlineShop/ShopManagement.Domain/ProductAgg/IProductRepository.cs
@@ -6,6 +6,7 @@ namespace ShopManagement.Domain.ProductAgg
 {
     public interface IProductRepository : IRepsoitory<long , Product>
     {
+        Product GetProductWithCategory(long id);
         EditProduct GetDetails(long id);
         List<ProductViewModel> Search(ProductSearchModel searchModel);
         List<ProductViewModel> GetProducts();
