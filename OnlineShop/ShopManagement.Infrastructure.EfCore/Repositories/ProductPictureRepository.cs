cf43b906f34706d8ba8d423671360751713f04fe
Parsa <mehdipourparsa@gmail.com>
2021-08-13 19:14:17 +0430

    refactor : add get with product and category method to product picture repository class

--- OnlineShop/ShopManagement.Infrastructure.EfCore/Repositories/ProductPictureRepository.cs
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/Repositories/ProductPictureRepository.cs
@@ -23,11 +23,18 @@ namespace ShopManagement.Infrastructure.EfCore.Repositories
             return _context.ProductPictures.Select(p => new EditProductPicture()
             {
                 Id = p.Id,
-                Picture = p.Picture,
                 PictureAlt = p.PictureAlt,
                 PictureTitle = p.PictureTitle,
                 ProductId = p.ProductId
             }).FirstOrDefault(p => p.Id == id);
+        }
+
+        public ProductPicture GetWithProductAndCategory(long id)
+        {
+            return _context.ProductPictures
+                .Include(x => x.Product)
+                .ThenInclude(x => x.ProductCategory)
+                .FirstOrDefault(x=>x.Id == id);
         }
 
         public List<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel)
