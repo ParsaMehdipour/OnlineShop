f83262d6c58880bf908af49a102ccdba617b60a6
Parsa <mehdipourparsa@gmail.com>
2021-08-13 19:13:12 +0430

    refactor : change picture type from string to i from file , add some attributes

--- OnlineShop/ShopManagement.Application.Contracts/Slider/CreateSlider.cs
+++ OnlineShop/ShopManagement.Application.Contracts/Slider/CreateSlider.cs
@@ -1,14 +1,32 @@
-﻿namespace ShopManagement.Application.Contracts.Slider
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
+
+namespace ShopManagement.Application.Contracts.Slider
 {
     public class CreateSlider
     {
-        public string Picture { get; set; }
+        public IFormFile Picture { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Heading { get; set; }
-        public string Text { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string BtnText { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Link { get; set; }
     }
 }
