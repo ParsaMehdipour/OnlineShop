9185f44c7ebde95bb095c9b91a7610ac5ba8c1d8
Parsa <mehdipourparsa@gmail.com>
2021-08-13 19:12:56 +0430

    refactor : change picture type from string to i from file

--- OnlineShop/ShopManagement.Application.Contracts/ProductPicture/CreateProductPicture.cs
+++ OnlineShop/ShopManagement.Application.Contracts/ProductPicture/CreateProductPicture.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
 using ShopManagement.Application.Contracts.Product;
 
 namespace ShopManagement.Application.Contracts.ProductPicture
@@ -11,7 +12,8 @@ namespace ShopManagement.Application.Contracts.ProductPicture
         public long ProductId { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
-        public string Picture { get; set; }
+        [MaxFileSize(1024 * 1024,ErrorMessage = ValidationMessages.MaxFileSize)]
+        public IFormFile Picture { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
