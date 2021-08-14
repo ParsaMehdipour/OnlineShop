f41530889b8a170b86b7220f12be266e4d1ca5d5
Parsa <mehdipourparsa@gmail.com>
2021-08-14 22:54:13 +0430

    feat : add i article category repository interface

---
+++ OnlineShop/ArticleManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using ArticleManagement.Application.Contracts.ArticleCategory;
+
+namespace ArticleManagement.Domain.ArticleCategoryAgg
+{
+    public interface IArticleCategoryRepository : IRepsoitory<long,ArticleCategory>
+    {
+        EditArticleCategory GetDetails(long id);
+        List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
+    }
+}
