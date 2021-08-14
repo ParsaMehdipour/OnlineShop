190bdc50ede1d9a987cc3ca6fab3807a2e235c14
Parsa <mehdipourparsa@gmail.com>
2021-08-14 22:55:30 +0430

    feat : add article category view model class

---
+++ OnlineShop/ArticleManagement.Application.Contracts/ArticleCategory/ArticleCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ArticleManagement.Application.Contracts.ArticleCategory
+{
+    public class ArticleCategoryViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string CreationDate { get; set; }
+
+    }
+}
