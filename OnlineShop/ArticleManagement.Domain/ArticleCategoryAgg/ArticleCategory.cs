cc272a1828b907fb4200cf13f05cd55222507010
Parsa <mehdipourparsa@gmail.com>
2021-08-14 22:53:55 +0430

    feat : add article category domain class

---
+++ OnlineShop/ArticleManagement.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,46 @@
+﻿using _0_Framework.Domain;
+
+namespace ArticleManagement.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory :BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Picture { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+        public string Slug { get; private set; }
+        public string MetaDescription { get; private set; }
+        public int ShowOrder { get; private set; }
+        public string CanonicalAddress { get; private set; }
+        public string Keywords { get; private set; }
+
+        public ArticleCategory(string name, string description, string picture, string pictureAlt, string pictureTitle, string slug, string metaDescription, int showOrder, string canonicalAddress, string keywords)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Slug = slug;
+            MetaDescription = metaDescription;
+            ShowOrder = showOrder;
+            CanonicalAddress = canonicalAddress;
+            Keywords = keywords;
+        }
+
+        public void Edit(string name, string description, string picture, string pictureAlt, string pictureTitle, string slug, string metaDescription, int showOrder, string canonicalAddress, string keywords)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Slug = slug;
+            MetaDescription = metaDescription;
+            ShowOrder = showOrder;
+            CanonicalAddress = canonicalAddress;
+            Keywords = keywords;
+        }
+    }
+}
