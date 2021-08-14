04efe878d81ba9c338a5338acf10afa9a72cc680
Parsa <mehdipourparsa@gmail.com>
2021-08-14 22:54:58 +0430

    feat : add create article category class

---
+++ OnlineShop/ArticleManagement.Application.Contracts/ArticleCategory/CreateArticleCategory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ArticleManagement.Application.Contracts.ArticleCategory
+{
+    public class CreateArticleCategory
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public string Slug { get; set; }
+        public string MetaDescription { get; set; }
+        public int ShowOrder { get; set; }
+        public string CanonicalAddress { get; set; }
+        public string Keywords { get; set; }
+    }
+}
