5f8aac769dafbabe57ea02a0b265ef2aaca442d2
Parsa <mehdipourparsa@gmail.com>
2021-08-14 22:55:11 +0430

    feat : add edit article category class

---
+++ OnlineShop/ArticleManagement.Application.Contracts/ArticleCategory/EditArticleCategory.cs
@@ -0,0 +1,7 @@
+﻿namespace ArticleManagement.Application.Contracts.ArticleCategory
+{
+    public class EditArticleCategory : CreateArticleCategory
+    {
+        public long Id { get; set; }
+    }
+}
