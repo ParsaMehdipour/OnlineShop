e6cfb2c2d26dbbb52c4687eb378c1fc8b5376623
Parsa <mehdipourparsa@gmail.com>
2021-08-15 23:38:49 +0430

    feat : add article context

---
+++ OnlineShop/ArticleManagement.Infrastructure.EfCore/ArticleContext.cs
@@ -0,0 +1,23 @@
+﻿using ArticleManagement.Domain.ArticleCategoryAgg;
+using BlogManagement.Infrastructure.EFCore.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticleManagement.Infrastructure.EfCore
+{
+    public class ArticleContext :DbContext
+    {
+        public DbSet<ArticleCategory> ArticleCategories { get; set; }
+
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(ArticleCategoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
