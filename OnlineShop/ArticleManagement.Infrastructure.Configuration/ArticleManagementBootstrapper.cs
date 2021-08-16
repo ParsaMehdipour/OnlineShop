9713984f88167edfa1b8917b97088d8e528e0f32
Parsa <mehdipourparsa@gmail.com>
2021-08-16 14:36:48 +0430

    feat : add article management bootstrapper class

---
+++ OnlineShop/ArticleManagement.Infrastructure.Configuration/ArticleManagementBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using ArticleCategory.Application.ArticleCategory;
+using ArticleManagement.Application.Contracts.ArticleCategory;
+using ArticleManagement.Domain.ArticleCategoryAgg;
+using ArticleManagement.Infrastructure.EfCore;
+using ArticleManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArticleManagement.Infrastructure.Configuration
+{
+    public class ArticleManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
+            services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
+
+            services.AddDbContext<ArticleContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
