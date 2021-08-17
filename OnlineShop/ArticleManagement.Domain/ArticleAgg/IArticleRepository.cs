ba262c28be8a8eb88ddc2d02f57baba8fd678077
Parsa <mehdipourparsa@gmail.com>
2021-08-17 15:41:54 +0430

    feat : add i article repository interface

---
+++ OnlineShop/ArticleManagement.Domain/ArticleAgg/IArticleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using ArticleManagement.Application.Contracts.Article;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace ArticleManagement.Domain.ArticleAgg
+{
+    public interface IArticleRepository : IRepsoitory<long, Article>
+    {
+        EditArticle GetDetails(long id);
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+    }
+}
