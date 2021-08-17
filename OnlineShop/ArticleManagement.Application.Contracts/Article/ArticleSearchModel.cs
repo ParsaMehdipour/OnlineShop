b78b82322a0e073bd6eee6e23c6639350dbbae73
Parsa <mehdipourparsa@gmail.com>
2021-08-17 15:41:07 +0430

    feat : add article search model class

---
+++ OnlineShop/ArticleManagement.Application.Contracts/Article/ArticleSearchModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ArticleManagement.Application.Contracts.Article
+{
+    public class ArticleSearchModel
+    {
+        public string Title { get; set; }
+        public long CategoryId { get; set; }
+    }
+}
