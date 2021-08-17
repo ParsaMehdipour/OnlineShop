c7e9705c759fadb4dd33283b4f180556ec6519b7
Parsa <mehdipourparsa@gmail.com>
2021-08-17 15:41:27 +0430

    feat : add i article application interface

---
+++ OnlineShop/ArticleManagement.Application.Contracts/Article/IArticleApplication.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace ArticleManagement.Application.Contracts.Article
+{
+    public interface IArticleApplication
+    {
+        OperationResult Create(CreateArticle command);
+        OperationResult Edit(EditArticle command);
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+        EditArticle GetDetails(long id);
+    }
+}
