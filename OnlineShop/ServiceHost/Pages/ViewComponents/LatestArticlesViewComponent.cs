719781f581994160ad374b64295c12e480d0fffa
Parsa <mehdipourparsa@gmail.com>
2021-08-21 16:45:34 +0430

    feat : add latest articles view component class

---
+++ OnlineShop/ServiceHost/Pages/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using _01_OnlineShopQuery.Contracts.Article;
+using _01_OnlineShopQuery.Contracts.Product;
+using _01_OnlineShopQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Pages.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        private readonly IArticleQuery _query;
+
+        public LatestArticlesViewComponent(IArticleQuery query)
+        {
+            _query = query;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var query = _query.LatestArticles();
+
+            return View(query);
+        }
+    }
+}
