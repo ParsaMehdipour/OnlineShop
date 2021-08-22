5ef6859687d87e3e4f9ad7dc23217ff0c06584ac
Parsa <mehdipourparsa@gmail.com>
2021-08-22 21:25:42 +0430

    refactor : add get details method to i product query interface

--- OnlineShop/01_OnlineShopQuery/Contracts/Product/IProductQuery.cs
+++ OnlineShop/01_OnlineShopQuery/Contracts/Product/IProductQuery.cs
@@ -4,6 +4,7 @@ namespace _01_OnlineShopQuery.Contracts.Product
 {
     public interface IProductQuery
     {
+        ProductQueryModel GetDetails(string slug);
         List<ProductQueryModel> GetLatestProducts();
         List<ProductQueryModel> Search(string value);
     }
