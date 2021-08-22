d38f12d2068d0520b024c13f180b742d01705a90
Parsa <mehdipourparsa@gmail.com>
2021-08-22 21:25:57 +0430

    refactor : add get details method to product query class

--- OnlineShop/01_OnlineShopQuery/Query/Product/ProductQuery.cs
+++ OnlineShop/01_OnlineShopQuery/Query/Product/ProductQuery.cs
@@ -16,19 +16,79 @@ namespace _01_OnlineShopQuery.Query.Product
         private readonly DiscountContext _discountContext;
         private readonly InventoryContext _inventoryContext;
 
-        public ProductQuery(ShopContext shopContext,DiscountContext discountContext,InventoryContext inventoryContext)
+        public ProductQuery(ShopContext shopContext, DiscountContext discountContext, InventoryContext inventoryContext)
         {
             _shopContext = shopContext;
             _discountContext = discountContext;
             _inventoryContext = inventoryContext;
         }
 
+        public ProductQueryModel GetDetails(string slug)
+        {
+            var inventories = _inventoryContext.Inventory.Select(x => new { x.UnitPrice, x.ProductId, x.InStock }).ToList();
+            var discounts = _discountContext.CustomerDiscounts
+                .Where(x => x.StartDate < DateTime.Now && x.EndDate > DateTime.Now)
+                .Select(x => new { x.DiscountRate, x.ProductId, x.EndDate }).ToList();
+
+            var product = _shopContext.Products
+                .Include(x => x.ProductCategory)
+                .Select(x => new ProductQueryModel
+                {
+                    Id = x.Id,
+                    Category = x.ProductCategory.Name,
+                    Name = x.Name,
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    Slug = x.Slug,
+                    CategorySlug = x.ProductCategory.Slug,
+                    Code = x.Code,
+                    Description = x.Description,
+                    Keywords = x.Keywords,
+                    MetaDescription = x.MetaDescription,
+                    ShortDescription = x.ShortDescription,
+                }).AsNoTracking().FirstOrDefault(x => x.Slug == slug);
+
+            if (product == null)
+                return new ProductQueryModel();
+
+
+            var productInventory = inventories.FirstOrDefault(x => x.ProductId == product.Id);
+
+            if (productInventory != null)
+            {
+                product.IsInStock = productInventory.InStock;
+
+                var price = productInventory.UnitPrice;
+
+                product.Price = price.ToMoney();
+
+                var productDiscount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
+
+                if (productDiscount != null)
+                {
+                    var discountRate = productDiscount.DiscountRate;
+
+                    product.DiscountExpirationDate = productDiscount.EndDate.ToDiscountFormat();
+                    product.DiscountRate = discountRate;
+                    product.HasDiscount = discountRate > 0;
+
+                    var discountAmount = Math.Round((price * discountRate) / 100);
+
+                    product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                }
+            }
+
+
+            return product;
+        }
+
         public List<ProductQueryModel> GetLatestProducts()
         {
-            var inventories = _inventoryContext.Inventory.Select(x => new {x.UnitPrice, x.ProductId}).ToList();
+            var inventories = _inventoryContext.Inventory.Select(x => new { x.UnitPrice, x.ProductId }).ToList();
             var discounts = _discountContext.CustomerDiscounts
-                .Where(x=> x.StartDate < DateTime.Now && x.EndDate > DateTime.Now)
-                .Select(x => new {x.DiscountRate, x.ProductId}).ToList();
+                .Where(x => x.StartDate < DateTime.Now && x.EndDate > DateTime.Now)
+                .Select(x => new { x.DiscountRate, x.ProductId }).ToList();
 
             var products = _shopContext.Products
                 .Include(x => x.ProductCategory)
