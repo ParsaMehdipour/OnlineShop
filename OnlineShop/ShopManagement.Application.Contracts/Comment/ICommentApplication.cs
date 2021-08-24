f46c9a4d6fe573938722f02730accbf7309b62cc
Parsa <mehdipourparsa@gmail.com>
2021-08-24 22:30:40 +0430

    feat : add i comment application interface

---
+++ OnlineShop/ShopManagement.Application.Contracts/Comment/ICommentApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.Comment
+{
+    public interface ICommentApplication
+    {
+        OperationResult Create(CreateComment command);
+        OperationResult Confirm(long id);
+        OperationResult Cancel(long id);
+        List<CommentViewModel> Search(CommentSearchModel searchModel);
+    }
+}
