4bc072416e94db80c3ce69d0c444daab3f37a2e1
Parsa <mehdipourparsa@gmail.com>
2021-08-24 22:34:07 +0430

    refactor : add comment navigation

--- OnlineShop/ShopManagement.Infrastructure.EfCore/Mapping/ProductMapping.cs
+++ OnlineShop/ShopManagement.Infrastructure.EfCore/Mapping/ProductMapping.cs
@@ -45,6 +45,10 @@ namespace ShopManagement.Infrastructure.EfCore.Mapping
             builder.HasMany(x => x.ProductPictures)
                 .WithOne(x => x.Product)
                 .HasForeignKey(x => x.ProductId);
+
+            builder.HasMany(x => x.Comments)
+                .WithOne(x => x.Product)
+                .HasForeignKey(x => x.ProductId);
         }
     }
 }
