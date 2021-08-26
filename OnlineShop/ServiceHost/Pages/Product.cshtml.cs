16d6739d75208b0baac900990bb6db90493a5e35
Parsa <mehdipourparsa@gmail.com>
2021-08-26 19:46:51 +0430

    refactor : add on post handler for comments

--- OnlineShop/ServiceHost/Pages/Product.cshtml.cs
+++ OnlineShop/ServiceHost/Pages/Product.cshtml.cs
@@ -1,21 +1,32 @@
 using _01_OnlineShopQuery.Contracts.Product;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.Comment;
 
 namespace ServiceHost.Pages
 {
     public class ProductModel : PageModel
     {
         private readonly IProductQuery _query;
+        private readonly ICommentApplication _commentApplication;
         public ProductQueryModel Product { get; set; }
 
-        public ProductModel(IProductQuery query)
+        public ProductModel(IProductQuery query,ICommentApplication commentApplication)
         {
             _query = query;
+            _commentApplication = commentApplication;
         }
 
         public void OnGet(string id)
         {
             Product = _query.GetDetails(id);
+        }
+
+        public IActionResult OnPost(CreateComment command,string productSlug)
+        {
+            _commentApplication.Create(command);
+
+            return new RedirectToPageResult("/Product", new {Id = productSlug});
         }
     }
 }
