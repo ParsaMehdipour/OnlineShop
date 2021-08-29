247b504d72fbe5e5fd618b5283851013bc25b800
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-08-29 23:14:56 +0430

    refactor : add get article method to article query class

--- OnlineShop/01_OnlineShopQuery/Query/Article/ArticleQuery.cs
+++ OnlineShop/01_OnlineShopQuery/Query/Article/ArticleQuery.cs
@@ -31,5 +31,28 @@ namespace _01_OnlineShopQuery.Query.Article
                     ShortDescription = x.ShortDescription,
                 }).ToList();
         }
+
+        public ArticleQueryModel GetArticle(string slug)
+        {
+            return _context.Articles
+                .Include(x => x.ArticleCategory)
+                .Select(x => new ArticleQueryModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    Description = x.Description,
+                    ArticleCategory = x.ArticleCategory.Name,
+                    CategoryId = x.ArticleCategory.Id,
+                    PublishDate = x.PublishDate.ToFarsi(),
+                    ShortDescription = x.ShortDescription,
+                    MetaDescription = x.MetaDescription,
+                    Keywords = x.Keywords,
+                    CanonicalAddress = x.CanonicalAddress
+                }).FirstOrDefault(x => x.Slug == slug);
+        }
     }
 }
