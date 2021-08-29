5f6ce165d3cd5d3a91e5efb52d345c75acec647a
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-08-29 23:15:43 +0430

    feat : add article category class

---
+++ OnlineShop/01_OnlineShopQuery/Query/Article/ArticleCategoryQuery.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using _01_OnlineShopQuery.Contracts.ArticleCategory;
+using ArticleManagement.Infrastructure.EfCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _01_OnlineShopQuery.Query.Article
+{
+    public class ArticleCategoryQuery : IArticleCategoryQuery
+    {
+        private readonly ArticleContext _context;
+
+        public ArticleCategoryQuery(ArticleContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticleCategoryQueryModel> GetAllArticleCategories()
+        {
+            return _context.ArticleCategories
+                .Include(x => x.Articles)
+                .Select(x => new ArticleCategoryQueryModel
+                {
+                    Name = x.Name,
+                    Slug = x.Slug,
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    ArticlesCount = x.Articles.Count
+                }).ToList();
+        }
+    }
+}
