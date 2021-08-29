e132dc1478120604b7f65fd7e59090d555de2a32
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-08-29 23:15:09 +0430

    feat : add article category query model

---
+++ OnlineShop/01_OnlineShopQuery/Contracts/ArticleCategory/ArticleCategoryQueryModel.cs
@@ -0,0 +1,17 @@
+﻿namespace _01_OnlineShopQuery.Contracts.ArticleCategory
+{
+    public class ArticleCategoryQueryModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public string Slug { get; set; }
+        public string MetaDescription { get; set; }
+        public int ShowOrder { get; set; }
+        public int ArticlesCount { get; set; }
+        public string CanonicalAddress { get; set; }
+        public string Keywords { get; set; }
+    }
+}
