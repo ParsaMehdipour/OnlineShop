a434a8b7ebc3a1ca5a4275c3c723254febcecba6
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-08-31 14:25:50 +0430

    refactor : add get article category method to article category query class

--- OnlineShop/01_OnlineShopQuery/Query/Article/ArticleCategoryQuery.cs
+++ OnlineShop/01_OnlineShopQuery/Query/Article/ArticleCategoryQuery.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using _0_Framework.Application;
+using _01_OnlineShopQuery.Contracts.Article;
 using _01_OnlineShopQuery.Contracts.ArticleCategory;
 using ArticleManagement.Infrastructure.EfCore;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +30,45 @@ namespace _01_OnlineShopQuery.Query.Article
                     PictureTitle = x.PictureTitle,
                     ArticlesCount = x.Articles.Count
                 }).ToList();
+        }
+
+        public ArticleCategoryQueryModel GetArticleCategory(string slug)
+        {
+            var articleCategory = _context.ArticleCategories
+                .Include(x => x.Articles)
+                .Select(x => new ArticleCategoryQueryModel
+                {
+                    Slug = x.Slug,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    Keywords = x.Keywords,
+                    MetaDescription = x.MetaDescription,
+                    CanonicalAddress = x.CanonicalAddress,
+                    ArticlesCount = x.Articles.Count,
+                    Articles = MapToArticles(x.Articles)
+                }).FirstOrDefault(x => x.Slug == slug);
+
+            if (!string.IsNullOrWhiteSpace(articleCategory.Keywords))
+                articleCategory.KeywordsList = articleCategory.Keywords.Split(",").ToList();
+
+            return articleCategory;
+        }
+
+        private static List<ArticleQueryModel> MapToArticles(List<ArticleManagement.Domain.ArticleAgg.Article> articles)
+        {
+            return articles.Select(x => new ArticleQueryModel
+            {
+                Slug = x.Slug,
+                ShortDescription = x.ShortDescription,
+                Title = x.Title,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                PublishDate = x.PublishDate.ToFarsi(),
+            }).ToList();
         }
     }
 }
