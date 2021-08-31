02c18f4001ac326e40d58497b5614918c704863e
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-08-31 14:27:15 +0430

    refactor : use validation attributes

--- OnlineShop/ArticleManagement.Application.Contracts/Article/CreateArticle.cs
+++ OnlineShop/ArticleManagement.Application.Contracts/Article/CreateArticle.cs
@@ -1,21 +1,40 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
 using Microsoft.AspNetCore.Http;
 
 namespace ArticleManagement.Application.Contracts.Article
 {
     public class CreateArticle
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Title { get; set; }
+
         public string Description { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(1000)]
         public string ShortDescription { get; set; }
+
+        [MaxFileSize(1024 * 1024 * 1024,ErrorMessage = ValidationMessages.MaxFileSize)]
         public IFormFile Picture { get; set; }
+
         public string PictureAlt { get; set; }
+
         public string PictureTitle { get; set; }
+
         public string Slug { get; set; }
+
         public string Keywords { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(1000)]
         public string MetaDescription { get; set; }
+
         public string CanonicalAddress { get; set; }
+
         public string PublishDate { get; set; }
+
         public long CategoryId { get; set; }
     }
 }
