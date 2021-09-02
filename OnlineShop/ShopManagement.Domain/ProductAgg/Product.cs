a269bf8f16cd45b66c1fa5798dfde15fa08e0eb9
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-02 21:51:36 +0430

    refactor : remove comment relation form product domain

--- OnlineShop/ShopManagement.Domain/ProductAgg/Product.cs
+++ OnlineShop/ShopManagement.Domain/ProductAgg/Product.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using _0_Framework.Domain;
-using _0_Framework.Infrastructure;
-using ShopManagement.Domain.CommentAgg;
 using ShopManagement.Domain.ProductCategoryAgg;
 using ShopManagement.Domain.ProductPictureAgg;
 
@@ -22,7 +20,6 @@ namespace ShopManagement.Domain.ProductAgg
         public long CategoryId { get; private set; }
         public ProductCategory ProductCategory { get; private set; }
         public List<ProductPicture> ProductPictures { get; private set; }
-        public List<Comment> Comments { get; private set; }
 
         public Product()
         {
