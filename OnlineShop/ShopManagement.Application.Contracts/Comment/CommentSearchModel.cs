426b5f9642ad2557b4f0a02c07f1b45119766f5a
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-02 21:49:35 +0430

    remove : remove comment contracts from shop application contracts

--- OnlineShop/ShopManagement.Application.Contracts/Comment/CommentSearchModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShopManagement.Application.Contracts.Comment
-{
-    public class CommentSearchModel
-    {
-        public long ProductId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
