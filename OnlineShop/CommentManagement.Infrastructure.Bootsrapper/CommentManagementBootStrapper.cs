91122ea4a963cfc1c43e8adcacccef0934f15334
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-02 21:58:42 +0430

    feat : add comment bootstrapper class

---
+++ OnlineShop/CommentManagement.Infrastructure.Bootsrapper/CommentManagementBootStrapper.cs
@@ -0,0 +1,25 @@
+﻿using CommentManagement.Application.Comment;
+using CommentManagement.Application.Contracts.Comment;
+using CommentManagement.Domain.CommentAgg;
+using CommentManagement.Infrastructure.EfCore;
+using CommentManagement.Infrastructure.EfCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommentManagement.Infrastructure.Configuration
+{
+    public class CommentManagementBootStrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<ICommentRepository, CommentRepository>();
+
+            services.AddTransient<ICommentApplication, CommentApplication>();
+
+            services.AddDbContext<CommentContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
