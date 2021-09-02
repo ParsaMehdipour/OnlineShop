f61c357ff55a4feafbc013fec5c52949518ceea2
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-02 21:54:38 +0430

    feat : add i comment repository interface

---
+++ OnlineShop/CommentManagement.Domain/CommentAgg/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Domain;
+using CommentManagement.Application.Contracts.Comment;
+
+namespace CommentManagement.Domain.CommentAgg
+{
+    public interface ICommentRepository : IRepsoitory<long,Comment>
+    {
+        List<CommentViewModel> Search(CommentSearchModel searchModel);
+    }
+}
