b568e1f129153207a158180ec49f23790691032f
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-05 16:03:18 +0430

    feat : add comment query model

---
+++ OnlineShop/01_OnlineShopQuery/Contracts/Comment/CommentQueryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_OnlineShopQuery.Contracts.Comment
+{
+    public class CommentQueryModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
