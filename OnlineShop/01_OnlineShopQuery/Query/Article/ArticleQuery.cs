a76a3547b7a41339a14a0859a11ee14e5dee40b2
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-06 18:24:06 +0430

    refactor : get comments for article query

--- OnlineShop/01_OnlineShopQuery/Query/Article/ArticleQuery.cs
+++ OnlineShop/01_OnlineShopQuery/Query/Article/ArticleQuery.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using _0_Framework.Application;
 using _01_OnlineShopQuery.Contracts.Article;
+using _01_OnlineShopQuery.Contracts.Comment;
 using ArticleManagement.Infrastructure.EfCore;
+using CommentManagement.Infrastructure.EfCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace _01_OnlineShopQuery.Query.Article
@@ -10,10 +12,12 @@ namespace _01_OnlineShopQuery.Query.Article
     public class ArticleQuery : IArticleQuery
     {
         private readonly ArticleContext _context;
+        private readonly CommentContext _commentContext;
 
-        public ArticleQuery(ArticleContext context)
+        public ArticleQuery(ArticleContext context,CommentContext commentContext)
         {
             _context = context;
+            _commentContext = commentContext;
         }
 
         public List<ArticleQueryModel> LatestArticles()
@@ -58,6 +62,28 @@ namespace _01_OnlineShopQuery.Query.Article
 
             if (!string.IsNullOrWhiteSpace(article.Keywords))
                 article.KeywordsList = article.Keywords.Split(",").ToList();
+
+            var comments = _commentContext.Comments
+                .Where(x => x.Type == CommentType.Article)
+                .Where(x => x.IsConfirmed == true)
+                .Where(x => x.IsCanceled == false)
+                .Where(x => x.OwnerRecordId == article.Id)
+                .Select(x => new CommentQueryModel
+                {
+                    Id = x.Id,
+                    CreationDate = x.CreationDate.ToFarsi(),
+                    Message = x.Message,
+                    Name = x.Name,
+                    ParentId = x.ParentId
+                }).OrderByDescending(x => x.Id).ToList();
+
+            foreach (var comment in comments)
+            {
+                if (comment.ParentId > 0)
+                    comment.ParentName = comments.FirstOrDefault(x => x.Id == comment.ParentId)?.Name;
+            }
+
+            article.Comments = comments;
 
             return article;
         }
