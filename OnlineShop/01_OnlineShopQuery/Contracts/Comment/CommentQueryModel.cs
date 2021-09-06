8116c3b88c2464014a8f605f65fc6980a03da1d5
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-06 18:23:46 +0430

    refactor : add missing properties

--- OnlineShop/01_OnlineShopQuery/Contracts/Comment/CommentQueryModel.cs
+++ OnlineShop/01_OnlineShopQuery/Contracts/Comment/CommentQueryModel.cs
@@ -5,5 +5,8 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public string Message { get; set; }
+        public string ParentName { get; set; }
+        public long ParentId { get; set; }
+        public string CreationDate { get; set; }
     }
 }
