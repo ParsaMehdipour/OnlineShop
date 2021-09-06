a953cb3d0561186a67672faf02e5509751f83081
Parsa Mehdipour <mehdipourparsa@gmail.com>
2021-09-06 18:25:35 +0430

    refactor : add on post handler for comments to article page model

--- OnlineShop/ServiceHost/Pages/Article.cshtml.cs
+++ OnlineShop/ServiceHost/Pages/Article.cshtml.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using _01_OnlineShopQuery.Contracts.Article;
 using _01_OnlineShopQuery.Contracts.ArticleCategory;
+using CommentManagement.Application.Contracts.Comment;
+using CommentManagement.Infrastructure.EfCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Pages
@@ -9,14 +12,16 @@ namespace ServiceHost.Pages
     {
         private readonly IArticleQuery _query;
         private readonly IArticleCategoryQuery _articleCategoryQuery;
+        private readonly ICommentApplication _commentApplication;
         public ArticleQueryModel Article { get; set; }
         public List<ArticleQueryModel> Articles { get; set; }
         public List<ArticleCategoryQueryModel> ArticleCategories { get; set; }
 
-        public ArticleModel(IArticleQuery query , IArticleCategoryQuery articleCategoryQuery)
+        public ArticleModel(IArticleQuery query , IArticleCategoryQuery articleCategoryQuery,ICommentApplication commentApplication)
         {
             _query = query;
             _articleCategoryQuery = articleCategoryQuery;
+            _commentApplication = commentApplication;
         }
 
         public void OnGet(string id)
@@ -24,6 +29,14 @@ namespace ServiceHost.Pages
             Article = _query.GetArticle(id);
             ArticleCategories = _articleCategoryQuery.GetAllArticleCategories();
             Articles = _query.LatestArticles();
+        }
+
+        public IActionResult OnPost(CreateComment command, string slug)
+        {
+            command.Type = CommentType.Article;
+            _commentApplication.Create(command);
+            return new RedirectToPageResult("/Article", new { Id = slug });
+
         }
     }
 }
